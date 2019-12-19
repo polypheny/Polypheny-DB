3b9279bf68e0180e7e6e4fd939f7111992bbfec7
Datomo <dave.lengw@gmail.com>
2019-12-19 16:42:17 +0100

    new OccurenceMap for staticCache

--- core/src/main/java/ch/unibas/dmi/dbis/polyphenydb/statistic/StatisticColumn.java
+++ core/src/main/java/ch/unibas/dmi/dbis/polyphenydb/statistic/StatisticColumn.java
@@ -1,11 +1,13 @@
 package ch.unibas.dmi.dbis.polyphenydb.statistic;
 
 
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Observable;
 import java.util.SortedMap;
-import lombok.Getter;
+import java.util.TreeMap;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
@@ -18,15 +20,14 @@ import lombok.extern.slf4j.Slf4j;
 public class StatisticColumn<T extends Comparable<T>> extends Observable {
 
     private final int MAX_MOST_USED_WORDS = 5;
+    private final int CACHE_SIZE = 5;
     private final String id;
 
-    @Setter
-    private SortedMap<T, Integer> minCache;
+    private LimitedOccurrenceMap<T> minCache = new LimitedOccurrenceMap<T>( CACHE_SIZE );
     @Setter
     private T min;
 
-    @Setter
-    private SortedMap<T, Integer> maxCache;
+    private LimitedOccurrenceMap<T> maxCache = new LimitedOccurrenceMap<T>( Comparator.reverseOrder(), CACHE_SIZE );
     @Setter
     private T max;
 
@@ -82,7 +83,59 @@ public class StatisticColumn<T extends Comparable<T>> extends Observable {
         if ( val instanceof String ) {
             return;
         }
+        this.maxCache.put( val );
+        this.minCache.put( val );
+    }
 
+
+    /**
+     * Special implementation of a TreeMap to only allow a regulated size of entries
+     */
+    class LimitedOccurrenceMap<K> {
+
+        private int maxSize;
+        private TreeMap<K, Integer> map;
+
+
+        LimitedOccurrenceMap( int maxSize ) {
+            this.map = new TreeMap<>();
+            this.maxSize = maxSize;
+        }
+
+
+        LimitedOccurrenceMap( Comparator<? super K> comparator, int maxSize ) {
+            this.map = new TreeMap<>( comparator );
+            this.maxSize = maxSize;
+        }
+
+
+        public void put( K key ) {
+            if ( map.containsKey( key ) ) {
+                map.replace( key, map.get( key ) );
+            } else {
+                map.put( key, 1 );
+            }
+            if ( map.size() > maxSize ) {
+                map.remove( map.firstKey() );
+            }
+        }
+
+
+        public void putAll( List<K> keys ) {
+            keys.forEach( k -> {
+                if ( map.containsKey( k ) ) {
+                    map.replace( k, map.get( k ) );
+                } else {
+                    map.put( k, 1 );
+                }
+            } );
+
+            if ( map.size() > maxSize ) {
+                while(map.size() > maxSize){
+                    map.remove( map.lastKey() );
+                }
+            }
+        }
 
     }
 
