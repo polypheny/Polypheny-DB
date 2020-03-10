5b1abe1084761059d10a0c16d2ad158ab4053405
Jan Schönholz <jan.schoenholz@unibas.ch>
2020-03-10 10:43:23 +0100

    Fix a possible infinite recursion in cassandra

--- cassandra-adapter/src/main/java/org/polypheny/db/adapter/cassandra/util/CassandraUtils.java
+++ cassandra-adapter/src/main/java/org/polypheny/db/adapter/cassandra/util/CassandraUtils.java
@@ -17,7 +17,11 @@
 package org.polypheny.db.adapter.cassandra.util;
 
 
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 import org.polypheny.db.adapter.cassandra.CassandraFilter;
 import org.polypheny.db.adapter.cassandra.CassandraTable;
 import org.polypheny.db.adapter.cassandra.CassandraTableModify;
@@ -35,8 +39,19 @@ public class CassandraUtils {
      * @return the {@link CassandraTable} or <code>null</code> if not found.
      */
     public static CassandraTable getUnderlyingTable( RelSubset relSubset ) {
-        List<RelNode> rels = relSubset.getRelList();
-        for ( RelNode relNode : rels ) {
+        return getUnderlyingTable( relSubset.getRelList() );
+    }
+
+
+    private static CassandraTable getUnderlyingTable( List<RelNode> rels ) {
+        Set<RelNode> alreadyChecked = new HashSet<>();
+        Deque<RelNode> innerLevel = new LinkedList<>();
+
+        innerLevel.addAll( rels );
+
+        while ( ! innerLevel.isEmpty() ) {
+            RelNode relNode = innerLevel.pop();
+            alreadyChecked.add( relNode );
             if ( relNode instanceof CassandraTableScan ) {
                 return ((CassandraTableScan) relNode).cassandraTable;
             } else if ( relNode instanceof CassandraTableModify ) {
@@ -44,9 +59,10 @@ public class CassandraUtils {
             } else {
                 for ( RelNode innerNode: relNode.getInputs() ) {
                     if ( innerNode instanceof RelSubset ) {
-                        CassandraTable table = getUnderlyingTable( (RelSubset) innerNode );
-                        if ( table != null ) {
-                            return table;
+                        for ( RelNode possibleNewRel : ((RelSubset) innerNode).getRelList() ) {
+                            if ( ! alreadyChecked.contains( possibleNewRel ) ) {
+                                innerLevel.addLast( possibleNewRel );
+                            }
                         }
                     }
                 }
