5cc0f2cd4a72229a692f50c2dcfca81cdf8a5c8d
Marco Vogt <marco.vogt@unibas.ch>
2021-07-07 15:58:40 +0200

    Enable ComplexViewTest for postgres

--- dbms/src/test/java/org/polypheny/db/sql/view/ComplexViewTest.java
+++ dbms/src/test/java/org/polypheny/db/sql/view/ComplexViewTest.java
@@ -35,14 +35,13 @@ import org.polypheny.db.excluded.CassandraExcluded;
 import org.polypheny.db.excluded.CottontailExcluded;
 import org.polypheny.db.excluded.FileExcluded;
 import org.polypheny.db.excluded.MonetdbExcluded;
-import org.polypheny.db.excluded.PostgresqlExcluded;
 
 /*
  * Table and Queries from https://github.com/polypheny/OLTPBench/tree/polypheny/src/com/oltpbenchmark/benchmarks/tpch
  */
 @SuppressWarnings({ "SqlDialectInspection", "SqlNoDataSourceInspection" })
 @Slf4j
-@Category({ AdapterTestSuite.class, MonetdbExcluded.class, PostgresqlExcluded.class, CassandraExcluded.class })
+@Category({ AdapterTestSuite.class, MonetdbExcluded.class, CassandraExcluded.class })
 public class ComplexViewTest {
 
     private final static String DROP_TABLES_NATION = "DROP TABLE IF EXISTS nation";
