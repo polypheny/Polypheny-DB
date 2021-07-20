26db21ca305911954c47e32fddcdc42085adccb3
Vishal Dalwadi <dalwadivishal26@gmail.com>
2021-07-20 14:42:21 +0530

    Make Modifiers annotation repeatable

--- cql-interface/src/main/java/org/polypheny/cql/contextset/annotations/Modifiers.java
+++ cql-interface/src/main/java/org/polypheny/cql/contextset/annotations/Modifiers.java
@@ -17,19 +17,14 @@
 package org.polypheny.cql.contextset.annotations;
 
 import java.lang.annotation.ElementType;
-import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import org.polypheny.cql.contextset.ModifierType;
 
-@Repeatable( Modifiers.class )
 @Retention( RetentionPolicy.RUNTIME )
 @Target( ElementType.TYPE )
-public @interface Modifier {
+public @interface Modifiers {
 
-    String Name();
-    String DefaultValue();
-    ModifierType Type();
+    Modifier[] value();
 
 }
