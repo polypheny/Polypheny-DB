8fd70e33c0f26ea43d95a7fa2ab308fcac4ef5f8
Vishal Dalwadi <dalwadivishal26@gmail.com>
2021-08-09 08:00:46 +0530

    Reduce conversions; Move building code from CqlParser to CqlQueryBuilder; Remove CqlResource and use CqlQuery instead.

--- cql-interface/src/main/java/org/polypheny/cql/CqlQuery.java
+++ cql-interface/src/main/java/org/polypheny/cql/CqlQuery.java
@@ -16,146 +16,55 @@
 
 package org.polypheny.cql;
 
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicReference;
-import org.polypheny.cql.exception.InvalidMethodInvocation;
-import org.polypheny.cql.exception.InvalidModifierException;
-import org.polypheny.cql.exception.UnexpectedTypeException;
+import org.polypheny.cql.BooleanGroup.ColumnOpsBooleanOperator;
 import org.polypheny.cql.utils.Tree;
-import org.polypheny.cql.utils.Tree.NodeType;
-import org.polypheny.cql.utils.Tree.TraversalType;
+import org.polypheny.db.util.Pair;
 
-public class CqlResource {
+public class CqlQuery {
 
-    public final Tree<Combiner, Index> queryRelation;
-    public final QueryNode filters;
-    public final Map<String, Index> indexMapping;
-    public final List<SortSpecification> sortSpecifications;
+    public final Tree<Combiner, TableIndex> queryRelation;
+    public final Tree<BooleanGroup<ColumnOpsBooleanOperator>, Filter> filters;
+    public final Map<String, TableIndex> tableIndexMapping;
+    public final Map<String, ColumnIndex> columnIndexMapping;
+    public final List<Pair<ColumnIndex, Map<String, Modifier>>> sortSpecifications;
 
     /*
-        TODO: sorting : Sort Specifications.
+        TODO: projecting : Projection and Aggregation.
+        TODO: grouping : Group Specifications.
      */
 
-    private CqlResource( final Tree<Combiner, Index> queryRelation,
-            final QueryNode filters, final Map<String, Index> indexMapping, List<SortSpecification> sortSpecs ) {
+    public CqlQuery( final Tree<Combiner, TableIndex> queryRelation,
+            final Tree<BooleanGroup<ColumnOpsBooleanOperator>, Filter> filters,
+            final Map<String, TableIndex> tableIndexMapping, final Map<String, ColumnIndex> columnIndexMapping,
+            final List<Pair<ColumnIndex, Map<String, Modifier>>> sortSpecifications ) {
         this.queryRelation = queryRelation;
         this.filters = filters;
-        this.indexMapping = indexMapping;
-        this.sortSpecifications = sortSpecs;
+        this.tableIndexMapping = tableIndexMapping;
+        this.columnIndexMapping = columnIndexMapping;
+        this.sortSpecifications = sortSpecifications;
     }
 
 
-    public static CqlResource createCqlResource( CqlQuery cqlQuery ) {
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
 
-        Tree<Combiner, Index> queryRelation =
-                generateQueryRelation( cqlQuery.tableOperations, cqlQuery.filters, cqlQuery.indexMapping );
-
-        return new CqlResource( queryRelation, cqlQuery.filters, cqlQuery.indexMapping, cqlQuery.sortSpecs );
-    }
-
-
-    private static Tree<Combiner, Index> generateQueryRelation( QueryNode tableOperations, QueryNode filters,
-            HashMap<String, Index> indexMapping ) {
-
-        AtomicReference<Tree<Combiner, Index>> queryRelation = new AtomicReference<>();
-        AtomicReference<BooleanGroup> booleanGroup = new AtomicReference<>();
-        AtomicReference<Index> lastIndex = new AtomicReference<>();
-
-        if ( tableOperations != null ) {
-            tableOperations.traverse( TraversalType.INORDER, ( treeNode, nodeType, direction, frame ) -> {
-                try {
-                    if ( nodeType == NodeType.DESTINATION_NODE ) {
-                        if ( treeNode.isLeaf() ) {
-                            SearchClause searchClause = treeNode.getExternalNode();
-                            Index index = indexMapping.get( searchClause.searchTerm );
-                            Tree<Combiner, Index> node = new Tree<>( index );
-                            if ( queryRelation.get() == null ) {
-                                queryRelation.set( node );
-                            } else {
-                                queryRelation.set(
-                                        new Tree<>( queryRelation.get(),
-                                                Combiner.createCombiner( booleanGroup.get(), lastIndex.get(), index ),
-                                                node )
-                                );
-                            }
-
-                            lastIndex.set( index );
-                        } else {
-                            booleanGroup.set(
-                                    treeNode.getInternalNode()
-                            );
-                        }
-                    }
-                } catch ( UnexpectedTypeException e ) {
-                    throw new RuntimeException( "This exception will never be thrown since checks have been made before"
-                            + " calling the getExternalNode and getInternalNode methods." );
-                } catch ( InvalidMethodInvocation e ) {
-                    // TODO
-                } catch ( InvalidModifierException e ) {
-                    // TODO
-                }
-
-                return true;
-            } );
-        } else {
-            HashSet<String> tablesSeen = new HashSet<>();
-
-            filters.traverse( TraversalType.INORDER, ( treeNode, nodeType, direction, frame ) -> {
-                try {
-                    if ( nodeType == NodeType.DESTINATION_NODE ) {
-                        if ( treeNode.isLeaf() ) {
-                            SearchClause searchClause = treeNode.getExternalNode();
-                            if ( !tablesSeen.contains( searchClause.indexStr ) ) {
-                                Index index = indexMapping.get( searchClause.indexStr );
-                                index = indexMapping.get( index.schemaName + "." + index.tableName );
-                                Tree<Combiner, Index> node = new Tree<>( index );
-                                if ( queryRelation.get() == null ) {
-                                    queryRelation.set( node );
-                                } else {
-                                    BooleanGroup currentBooleanGroup = new BooleanGroup( BooleanOperator.AND );
-                                    queryRelation.set(
-                                            new Tree<>( queryRelation.get(),
-                                                    Combiner.createCombiner( currentBooleanGroup, lastIndex.get(), index ),
-                                                    node )
-                                    );
-                                }
-                                tablesSeen.add( searchClause.indexStr );
-                                lastIndex.set( index );
-                            }
-
-                            if ( !tablesSeen.contains( searchClause.searchTerm ) ) {
-                                Index index = indexMapping.get( searchClause.searchTerm );
-                                if ( index != null ) {
-                                    index = indexMapping.get( index.schemaName + "." + index.tableName );
-                                    Tree<Combiner, Index> node = new Tree<>( index );
-                                    BooleanGroup currentBooleanGroup = new BooleanGroup( BooleanOperator.AND );
-                                    queryRelation.set(
-                                            new Tree<>( queryRelation.get(),
-                                                    Combiner.createCombiner( currentBooleanGroup, lastIndex.get(), index ),
-                                                    node )
-                                    );
-                                    tablesSeen.add( searchClause.searchTerm );
-                                    lastIndex.set( index );
-                                }
-                            }
-                        }
-                    }
-                } catch ( UnexpectedTypeException e ) {
-                    throw new RuntimeException( "This exception will never be thrown since checks have been made before"
-                            + " calling the getExternalNode method." );
-                } catch ( InvalidMethodInvocation e ) {
-                    // TODO
-                } catch ( InvalidModifierException e ) {
-                    // TODO
-                }
-
-                return true;
-            } );
+        if ( filters != null ) {
+            stringBuilder.append( filters );
+        }
+        stringBuilder.append( "relation " )
+                .append( queryRelation );
+        if ( sortSpecifications != null && sortSpecifications.size() != 0 ) {
+            stringBuilder.append( " " );
+            for ( Pair<ColumnIndex, Map<String, Modifier>> sortSpecification : sortSpecifications ) {
+                stringBuilder.append( sortSpecification.toString() );
+            }
+            stringBuilder.append( " " );
         }
 
-        return queryRelation.get();
+        return stringBuilder.toString();
     }
+
 }
---
+++ cql-interface/src/main/java/org/polypheny/cql/CqlQueryBuilder.java
@@ -0,0 +1,186 @@
+/*
+ * Copyright 2019-2021 The Polypheny Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.polypheny.cql;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Stack;
+import java.util.TreeMap;
+import org.polypheny.cql.BooleanGroup.ColumnOpsBooleanOperator;
+import org.polypheny.cql.BooleanGroup.TableOpsBooleanOperator;
+import org.polypheny.cql.exception.UnknownIndexException;
+import org.polypheny.cql.utils.Tree;
+import org.polypheny.db.util.Pair;
+
+public class CqlQueryBuilder {
+
+    private String databaseName;
+    private Tree<Combiner, TableIndex> queryRelation;
+    private Stack<Tree<BooleanGroup<ColumnOpsBooleanOperator>, Filter>> filters;
+    private Map<String, TableIndex> tableIndexMapping;
+    private Map<String, TableIndex> tableAliases;
+    private Map<String, ColumnIndex> columnIndexMapping;
+    private List<Pair<ColumnIndex, Map<String, Modifier>>> sortSpecifications;
+
+    private TableIndex lastTableIndex;
+
+
+    public CqlQueryBuilder( String databaseName ) {
+        this.databaseName = databaseName;
+        this.filters = new Stack<>();
+        this.tableIndexMapping = new TreeMap<>( String.CASE_INSENSITIVE_ORDER );
+        this.tableAliases = new TreeMap<>( String.CASE_INSENSITIVE_ORDER );
+        this.columnIndexMapping = new TreeMap<>( String.CASE_INSENSITIVE_ORDER );
+        this.sortSpecifications = new ArrayList<>();
+    }
+
+
+    public CqlQuery build() throws Exception {
+        if ( queryRelation == null && filters.size() == 0 ) {
+            throw new Exception( "Query relations and filters cannot be both empty." );
+        }
+
+        if ( queryRelation == null ) {
+            assert filters.size() == 1;
+
+            generateDefaultQueryRelation();
+        }
+
+        Tree<BooleanGroup<ColumnOpsBooleanOperator>, Filter> queryFilters = null;
+        if ( filters.size() == 1 ) {
+            queryFilters = filters.pop();
+        }
+
+        return new CqlQuery( queryRelation, queryFilters, tableIndexMapping, columnIndexMapping, sortSpecifications );
+    }
+
+
+    private void generateDefaultQueryRelation() {
+        boolean first = true;
+        tableIndexMapping.forEach( ( tableName, tableIndex ) -> {
+            if ( first ) {
+                addTable( tableIndex );
+            } else {
+                BooleanGroup<TableOpsBooleanOperator> innerJoin = new BooleanGroup<>( TableOpsBooleanOperator.AND );
+                combineRelationWith( tableIndex, innerJoin );
+            }
+        } );
+    }
+
+
+    public TableIndex addTableIndex ( String fullyQualifiedTableName )
+            throws UnknownIndexException {
+
+        String[] split = fullyQualifiedTableName.split( "\\." );
+
+        assert split.length == 2;
+
+        return addTableIndex( split[0], split[1] );
+    }
+
+
+    public TableIndex addTableIndex( String schemaName, String tableName ) throws UnknownIndexException {
+
+        String fullyQualifiedTableName = schemaName + "." + tableName;
+
+        if ( !this.tableIndexMapping.containsKey( fullyQualifiedTableName ) ) {
+            TableIndex tableIndex = TableIndex.createIndex( databaseName, schemaName, tableName );
+            this.tableIndexMapping.put( tableIndex.fullyQualifiedName, tableIndex );
+        }
+        return this.tableIndexMapping.get( fullyQualifiedTableName );
+    }
+
+
+    public ColumnIndex addColumnIndex( String fullyQualifiedColumnName )
+            throws UnknownIndexException {
+
+        String[] split = fullyQualifiedColumnName.split( "\\." );
+
+        assert split.length == 3;
+
+        return addColumnIndex( split[0], split[1], split[2] );
+    }
+
+
+    public ColumnIndex addColumnIndex( String schemaName, String tableName, String columnName )
+            throws UnknownIndexException {
+
+        addTableIndex( schemaName, tableName );
+
+        String fullyQualifiedColumnName = schemaName + "." + tableName + "." + columnName;
+        if ( !columnIndexMapping.containsKey( fullyQualifiedColumnName ) ) {
+            ColumnIndex columnIndex = ColumnIndex.createIndex( databaseName, schemaName, tableName, columnName );
+            columnIndexMapping.put( columnIndex.fullyQualifiedName, columnIndex );
+        }
+
+        return columnIndexMapping.get( fullyQualifiedColumnName );
+    }
+
+
+    public void addTable( TableIndex tableIndex ) {
+        assert this.queryRelation == null;
+
+        this.queryRelation = new Tree<>( tableIndex );
+        this.lastTableIndex = tableIndex;
+    }
+
+
+    public void combineRelationWith( TableIndex tableIndex, BooleanGroup<TableOpsBooleanOperator> booleanGroup ) {
+
+        assert this.queryRelation != null;
+
+        Combiner combiner = Combiner.createCombiner( booleanGroup, this.lastTableIndex, tableIndex );
+
+        this.queryRelation = new Tree<>(
+                this.queryRelation,
+                combiner,
+                new Tree<>( tableIndex )
+        );
+
+        this.lastTableIndex = tableIndex;
+    }
+
+
+    public void addNewFilter( Filter filter ) {
+        Tree<BooleanGroup<ColumnOpsBooleanOperator>, Filter> root = new Tree<>( filter );
+        this.filters.push( root );
+    }
+
+
+    public void mergeFilterSubtreesWith( BooleanGroup<ColumnOpsBooleanOperator> booleanGroup ) {
+        Tree<BooleanGroup<ColumnOpsBooleanOperator>, Filter> right = this.filters.pop();
+        Tree<BooleanGroup<ColumnOpsBooleanOperator>, Filter> left = this.filters.pop();
+        assert right != null;
+        assert left != null;
+
+        left = new Tree<>(
+                left,
+                booleanGroup,
+                right
+        );
+
+        this.filters.push( left );
+    }
+
+    public void addSortSpecification( ColumnIndex columnIndex, Map<String, Modifier> modifiers ) {
+        this.sortSpecifications.add(
+                new Pair<>( columnIndex, modifiers )
+        );
+    }
+}
