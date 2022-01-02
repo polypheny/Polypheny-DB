bc63c03abd0afe09244f786a1e74d7ea40ebb78a
Marco Vogt <marco.vogt@unibas.ch>
2022-01-02 11:21:23 +0100

    Update year in copyright

--- config/src/main/java/org/polypheny/db/config/ConfigBoolean.java
+++ config/src/main/java/org/polypheny/db/config/ConfigBoolean.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2021 The Polypheny Project
+ * Copyright 2019-2022 The Polypheny Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -64,8 +64,8 @@ public class ConfigBoolean extends ConfigScalar {
 
     /**
      * Checks if the currently set config value, is equal to the system configured default.
-     * If you want to reset it to the configured defaultValue use {@link #resetToDefault()}
-     * To change the systems default value you can use: {@link #changeDefaultValue(Object)}
+     * If you want to reset it to the configured defaultValue use {@link #resetToDefault()}.
+     * To change the systems default value you can use: {@link #changeDefaultValue(Object)}.
      *
      * @return true if it is set to default, false if it deviates
      */
@@ -76,10 +76,10 @@ public class ConfigBoolean extends ConfigScalar {
 
 
     /**
-     * Restores the current value to the system configured default vlaue.
+     * Restores the current value to the system configured default value.
      *
-     * To obtain the system configured defaultValue use {@link #getDefaultValue()}
-     * If you want to check if the current value deviates from default use:  {@link #isDefault()}.
+     * To obtain the system configured defaultValue use {@link #getDefaultValue()}.
+     * If you want to check if the current value deviates from default use: {@link #isDefault()}.
      */
     @Override
     public void resetToDefault() {
