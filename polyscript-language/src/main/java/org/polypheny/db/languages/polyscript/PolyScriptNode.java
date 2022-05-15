4d4bf21c99f00944520d855d903c296524143f52
Nicolas Odermatt <odermattn@gmail.com>
2022-05-15 16:12:29 +0200

    Make PolyScript parser importable

---
+++ polyscript-language/src/main/java/org/polypheny/db/languages/polyscript/PolyScriptNode.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2019-2022 The Polypheny Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.polypheny.db.languages.polyscript;
+
+import org.polypheny.db.algebra.constant.Kind;
+import org.polypheny.db.catalog.Catalog;
+import org.polypheny.db.languages.ParserPos;
+import org.polypheny.db.nodes.Node;
+import org.polypheny.db.nodes.NodeVisitor;
+import org.polypheny.db.util.Litmus;
+
+import java.util.Set;
+
+public class PolyScriptNode implements Node {
+    private final String statement;
+
+    public PolyScriptNode(String statement) {
+        this.statement = statement;
+    }
+
+    @Override
+    public Node clone(ParserPos pos) {
+        return null;
+    }
+
+    @Override
+    public Kind getKind() {
+        return null;
+    }
+
+    @Override
+    public Catalog.QueryLanguage getLanguage() {
+        return null;
+    }
+
+    @Override
+    public boolean isA(Set<Kind> category) {
+        return false;
+    }
+
+    @Override
+    public ParserPos getPos() {
+        return null;
+    }
+
+    @Override
+    public boolean equalsDeep(Node node, Litmus litmus) {
+        return false;
+    }
+
+    @Override
+    public <R> R accept(NodeVisitor<R> visitor) {
+        return null;
+    }
+}
