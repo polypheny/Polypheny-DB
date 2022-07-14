32bbf3c959847504b90137bf87aa4af439155177
Nicolas Odermatt <odermattn@gmail.com>
2022-07-14 22:16:25 +0200

    Parse named arguments within expressions

--- polyscript-language/src/main/java/org/polypheny/db/languages/polyscript/Expression.java
+++ polyscript-language/src/main/java/org/polypheny/db/languages/polyscript/Expression.java
@@ -1,15 +1,39 @@
 package org.polypheny.db.languages.polyscript;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Expression {
     private final String value;
 
 
+    private final List<String> namedArguments;
+    private final static Pattern NAMED_ARGUMENTS_PATTERN = Pattern.compile(":\\w+");
+
     public Expression(String value) {
         this.value = value;
+        this.namedArguments = parseNamedArguments();
+    }
+
+    private List<String> parseNamedArguments() {
+        List<String> namedArguments = new ArrayList<>();
+        final Matcher matcher = NAMED_ARGUMENTS_PATTERN.matcher(this.value);
+        while (matcher.find()) {
+            String argument = matcher.group(0);
+            namedArguments.add(argument.substring(1)); // strip prefix ':'
+        }
+        return namedArguments;
     }
 
     public String getValue() {
         return value;
+    }
+
+    public List<String> getNamedArguments() {
+        return Collections.unmodifiableList(namedArguments);
     }
 
     @Override
---
+++ polyscript-language/src/test/java/org/polypheny/db/languages/polyscript/ExpressionTest.java
@@ -0,0 +1,22 @@
+package org.polypheny.db.languages.polyscript;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class ExpressionTest {
+
+    @Test
+    void testExtractNamedArguments() {
+        String query = "sql(insert into students VALUES(:id, :name);";
+
+        Expression sut = new SqlExpression(query);
+
+        List<String> namedArguments = sut.getNamedArguments();
+        assertEquals(namedArguments.size(), 2);
+        assertEquals(namedArguments.get(0), "id");
+        assertEquals(namedArguments.get(1), "name");
+    }
+}
