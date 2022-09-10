ce8203c30d4ac0197e80d514a688d3c6819eb097
Nicolas <odermattn@gmail.com>
2022-09-10 13:53:18 +0200

    Fix SqlAdvisorTest

--- sql-language/src/test/java/org/polypheny/db/sql/sql/test/SqlAdvisorTest.java
+++ sql-language/src/test/java/org/polypheny/db/sql/sql/test/SqlAdvisorTest.java
@@ -123,6 +123,7 @@ public class SqlAdvisorTest extends SqlValidatorTestCase {
                     "KEYWORD(+)",
                     "KEYWORD(-)",
                     "KEYWORD(?)",
+                    "KEYWORD(:)",
                     "KEYWORD(ABS)",
                     "KEYWORD(ARRAY)",
                     "KEYWORD(AVG)",
