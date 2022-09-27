73b7fc1983c3aaab4eb6fcb30a72f84282f4b24d
datomo <dave.lengw@gmail.com>
2022-09-27 11:32:07 +0200

    removed sql-lang dependency from cypher, delete unused file

--- cypher-language/src/test/java/org/polypheny/db/CypherLanguageDependent.java
+++ cypher-language/src/test/java/org/polypheny/db/CypherLanguageDependent.java
@@ -1,17 +1,36 @@
 /*
- * Copyright 2019-2022 The Polypheny Project
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  * Copyright 2019-2022 The Polypheny Project
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  * http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *  *
+ *  * This file incorporates code covered by the following terms:
+ *  *
+ *  * Licensed to the Apache Software Foundation (ASF) under one or more
+ *  * contributor license agreements.  See the NOTICE file distributed with
+ *  * this work for additional information regarding copyright ownership.
+ *  * The ASF licenses this file to you under the Apache License, Version 2.0
+ *  * (the "License"); you may not use this file except in compliance with
+ *  * the License.  You may obtain a copy of the License at
+ *  *
+ *  * http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
  *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
 
 package org.polypheny.db;
@@ -52,7 +71,6 @@ import org.polypheny.db.schema.AggregateFunction;
 import org.polypheny.db.schema.Function;
 import org.polypheny.db.schema.TableFunction;
 import org.polypheny.db.schema.TableMacro;
-import org.polypheny.db.sql.SqlRegisterer;
 import org.polypheny.db.type.PolyIntervalQualifier;
 import org.polypheny.db.type.PolyType;
 import org.polypheny.db.type.checker.FamilyOperandTypeChecker;
@@ -67,9 +85,9 @@ import org.slf4j.Logger;
 public class CypherLanguageDependent extends LanguageManager {
 
     static {
-        if ( !SqlRegisterer.isInit() ) {
+        /*if ( !SqlRegisterer.isInit() ) {
             SqlRegisterer.registerOperators();
-        }
+        }*/
         if ( !CypherRegisterer.isInit() ) {
             CypherRegisterer.registerOperators();
         }
