50c248b61a745be1fc1f0772f9560f479a8b8788
Marco Vogt <marco.vogt@unibas.ch>
2023-02-06 10:56:13 +0100

    Update year in copyright

--- plugins/sql-language/src/main/java/org/polypheny/db/sql/language/dialect/package-info.java
+++ plugins/sql-language/src/main/java/org/polypheny/db/sql/language/dialect/package-info.java
@@ -1,6 +1,6 @@
 
 /*
- * Copyright 2019-2022 The Polypheny Project
+ * Copyright 2019-2023 The Polypheny Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
