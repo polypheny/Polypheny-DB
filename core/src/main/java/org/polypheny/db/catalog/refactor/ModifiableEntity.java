7c932ae907ed65ebc2060db380d6bb95e29eec7e
datomo <dave.lengw@gmail.com>
2023-02-25 12:50:30 +0100

    replaced algOpt entites with catalog entities

---
+++ core/src/main/java/org/polypheny/db/catalog/refactor/ModifiableEntity.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2019-2023 The Polypheny Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.polypheny.db.catalog.refactor;
+
+import java.util.List;
+import org.polypheny.db.algebra.AlgNode;
+import org.polypheny.db.algebra.core.common.Modify;
+import org.polypheny.db.algebra.core.common.Modify.Operation;
+import org.polypheny.db.catalog.entity.CatalogEntity;
+import org.polypheny.db.plan.AlgOptCluster;
+import org.polypheny.db.plan.AlgTraitSet;
+import org.polypheny.db.rex.RexNode;
+
+public interface ModifiableEntity {
+
+    Modify<?> toModificationAlg(
+            AlgOptCluster cluster,
+            AlgTraitSet traits,
+            CatalogEntity physicalEntity,
+            AlgNode child,
+            Operation operation,
+            List<String> targets,
+            List<RexNode> sources );
+
+
+}
