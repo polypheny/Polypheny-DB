bd8f80bc74cfe84cdc15bbde40803001c9fcecea
datomo <dave.lengw@gmail.com>
2023-03-01 16:02:21 +0100

    added logistic PolyCatalog functions

--- core/src/main/java/org/polypheny/db/catalog/entity/logical/LogicalNamespace.java
+++ core/src/main/java/org/polypheny/db/catalog/entity/logical/LogicalNamespace.java
@@ -26,14 +26,18 @@ import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.Value;
 import lombok.With;
+<<<<<<<< HEAD:core/src/main/java/org/polypheny/db/catalog/entity/logical/LogicalNamespace.java
 import org.polypheny.db.catalog.Catalog;
 import org.polypheny.db.catalog.entity.CatalogObject;
+========
+>>>>>>>> f595385dd (added logistic PolyCatalog functions):core/src/main/java/org/polypheny/db/catalog/entity/LogicalNamespace.java
 import org.polypheny.db.catalog.logistic.NamespaceType;
 
 
 @EqualsAndHashCode(callSuper = false)
 @With
 @Value
+<<<<<<<< HEAD:core/src/main/java/org/polypheny/db/catalog/entity/logical/LogicalNamespace.java
 public class LogicalNamespace implements CatalogObject, Comparable<LogicalNamespace> {
 
     private static final long serialVersionUID = 3090632164988970558L;
@@ -48,14 +52,35 @@ public class LogicalNamespace implements CatalogObject, Comparable<LogicalNamesp
     @EqualsAndHashCode.Exclude
     public NamespaceType namespaceType;
     @Serialize
+========
+public class LogicalNamespace extends CatalogNamespace implements CatalogObject, Comparable<LogicalNamespace> {
+
+    private static final long serialVersionUID = 3090632164988970558L;
+
+    public long id;
+    @Getter
+    public String name;
+    @Getter
+    @EqualsAndHashCode.Exclude
+    public NamespaceType namespaceType;
+
+>>>>>>>> f595385dd (added logistic PolyCatalog functions):core/src/main/java/org/polypheny/db/catalog/entity/LogicalNamespace.java
     public boolean caseSensitive;
 
 
     public LogicalNamespace(
+<<<<<<<< HEAD:core/src/main/java/org/polypheny/db/catalog/entity/logical/LogicalNamespace.java
             @Deserialize("id") final long id,
             @Deserialize("name") @NonNull final String name,
             @Deserialize("namespaceType") @NonNull final NamespaceType namespaceType,
             @Deserialize("caseSensitive") boolean caseSensitive ) {
+========
+            final long id,
+            @NonNull final String name,
+            @NonNull final NamespaceType namespaceType,
+            boolean caseSensitive ) {
+        super( id, name, namespaceType );
+>>>>>>>> f595385dd (added logistic PolyCatalog functions):core/src/main/java/org/polypheny/db/catalog/entity/LogicalNamespace.java
         this.id = id;
         this.name = name;
         this.namespaceType = namespaceType;
@@ -66,7 +91,11 @@ public class LogicalNamespace implements CatalogObject, Comparable<LogicalNamesp
     // Used for creating ResultSets
     @Override
     public Serializable[] getParameterArray() {
+<<<<<<<< HEAD:core/src/main/java/org/polypheny/db/catalog/entity/logical/LogicalNamespace.java
         return new Serializable[]{ name, Catalog.DATABASE_NAME, Catalog.USER_NAME, CatalogObject.getEnumNameOrNull( namespaceType ) };
+========
+        return new Serializable[]{ name, CatalogObject.getEnumNameOrNull( namespaceType ) };
+>>>>>>>> f595385dd (added logistic PolyCatalog functions):core/src/main/java/org/polypheny/db/catalog/entity/LogicalNamespace.java
     }
 
 
@@ -79,13 +108,11 @@ public class LogicalNamespace implements CatalogObject, Comparable<LogicalNamesp
         return -1;
     }
 
-
     @RequiredArgsConstructor
     public static class PrimitiveCatalogSchema {
 
         public final String tableSchem;
         public final String tableCatalog;
-        public final String owner;
         public final String schemaType;
 
     }
