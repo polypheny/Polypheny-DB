ef29693039b71c9fe2f97b664fd72ba94f1794ed
Tobias Hafner <mail@tobiashafner.ch>
2023-05-24 13:50:40 +0200

    add handler to handle exceptions thrown in rpcs

---
+++ plugins/proto-interface/src/main/java/org/polypheny/db/protointerface/ExceptionHandler.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2019-2023 The Polypheny Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.polypheny.db.protointerface;
+
+import io.grpc.*;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ExceptionHandler implements ServerInterceptor {
+    @Override
+    public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(ServerCall<ReqT, RespT> serverCall, Metadata metadata,
+                                                                 ServerCallHandler<ReqT, RespT> serverCallHandler) {
+        ServerCall.Listener<ReqT> listener = serverCallHandler.startCall(serverCall, metadata);
+        return new ExceptionHandlingServerCallListener<>(listener, serverCall, metadata);
+    }
+
+    private class ExceptionHandlingServerCallListener<ReqT, RespT> extends ForwardingServerCallListener.SimpleForwardingServerCallListener<ReqT> {
+        private final ServerCall<ReqT, RespT> serverCall;
+        private final Metadata metadata;
+
+        ExceptionHandlingServerCallListener(ServerCall.Listener<ReqT> listener, ServerCall<ReqT, RespT> serverCall, Metadata metadata) {
+            super(listener);
+            this.serverCall = serverCall;
+            this.metadata = metadata;
+        }
+
+        @Override
+        public void onHalfClose() {
+            try {
+                super.onReady();
+            } catch (RuntimeException e) {
+                handleException(e, serverCall, metadata);
+                throw e;
+            }
+        }
+
+        private void handleException(RuntimeException exception, ServerCall<ReqT, RespT> serverCall, Metadata metadata) {
+            if ( log.isErrorEnabled() ) {
+                log.trace("proto-interface encountered gRPC error: {}", exception.getMessage());
+            }
+            serverCall.close(Status.INTERNAL.withDescription(exception.getMessage()), metadata);
+        }
+    }
+}
