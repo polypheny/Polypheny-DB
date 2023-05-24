77b10fb4db99d899395cab63aab1b449a8d449c2
Tobias Hafner <mail@tobiashafner.ch>
2023-05-24 13:54:51 +0200

    add client management to proto interface

--- plugins/proto-interface/src/main/java/org/polypheny/db/protointerface/ClientManager.java
+++ plugins/proto-interface/src/main/java/org/polypheny/db/protointerface/ClientManager.java
@@ -16,12 +16,6 @@
 
 package org.polypheny.db.protointerface;
 
-import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.stream.Collectors;
-import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.polypheny.db.catalog.Catalog;
 import org.polypheny.db.catalog.entity.CatalogUser;
@@ -29,124 +23,78 @@ import org.polypheny.db.catalog.entity.logical.LogicalNamespace;
 import org.polypheny.db.iface.AuthenticationException;
 import org.polypheny.db.iface.Authenticator;
 import org.polypheny.db.protointerface.proto.ConnectionRequest;
-import org.polypheny.db.protointerface.utils.PropertyUtils;
 import org.polypheny.db.transaction.Transaction;
 import org.polypheny.db.transaction.TransactionException;
 import org.polypheny.db.transaction.TransactionManager;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 @Slf4j
 public class ClientManager {
-
-    private static final long HEARTBEAT_TOLERANCE = 2000;
-    @Getter
-    private long heartbeatInterval;
-
-    private ConcurrentHashMap<String, PIClient> openConnections;
+    private final String DEFAULT_USERNAME_KEY = "username";
+    private final String DEFAULT_PASSWORD_KEY = "password";
+    private ConcurrentHashMap<String, ProtoInterfaceClient> openConnections;
     private final Authenticator authenticator;
     private final TransactionManager transactionManager;
-    private Timer cleanupTimer;
 
-
-    public ClientManager( PIPlugin.ProtoInterface protoInterface ) {
-        this.openConnections = new ConcurrentHashMap<>();
-        this.authenticator = protoInterface.getAuthenticator();
-        this.transactionManager = protoInterface.getTransactionManager();
-        if ( protoInterface.isRequiresHeartbeat() ) {
-            this.heartbeatInterval = protoInterface.getHeartbeatIntervall();
-            this.cleanupTimer = new Timer();
-            cleanupTimer.schedule( createNewCleanupTask(), 0, heartbeatInterval + HEARTBEAT_TOLERANCE );
-        }
-        this.heartbeatInterval = 0;
+    public ClientManager(Authenticator authenticator, TransactionManager transactionManager) {
+        this.authenticator = authenticator;
+        this.transactionManager = transactionManager;
     }
 
-
-    public void unregisterConnection( PIClient client ) {
-        synchronized ( client ) {
-            client.prepareForDisposal();
-            openConnections.remove( client.getClientUUID() );
-        }
-    }
-
-
-    public void registerConnection( ConnectionRequest connectionRequest ) throws AuthenticationException, TransactionException, PIServiceException {
+    public void registerConnection(ConnectionRequest connectionRequest) throws AuthenticationException, TransactionException, ProtoInterfaceServiceException {
         if ( log.isTraceEnabled() ) {
-            log.trace( "User {} tries to establish connection via proto interface.", connectionRequest.getClientUuid() );
+            log.trace( "User {} tries to establish connection via proto interface.", connectionRequest.getClientUUID());
         }
         // reject already connected user
-        if ( isConnected( connectionRequest.getClientUuid() ) ) {
-            throw new PIServiceException( "A user with uid " + connectionRequest.getClientUuid() + "is already connected." );
+        if (isConnected(connectionRequest.getClientUUID())) {
+            throw new ProtoInterfaceServiceException("user with uid " + connectionRequest.getClientUUID() + "is already connected.");
         }
-        String username = connectionRequest.hasUsername() ? connectionRequest.getUsername() : Catalog.USER_NAME;
-        String password = connectionRequest.hasPassword() ? connectionRequest.getPassword() : null;
-        CatalogUser user = authenticator.authenticate( username, password );
-        Transaction transaction = transactionManager.startTransaction( user, null, false, "proto-interface" );
-        transaction.commit();
-        LogicalNamespace namespace = getNamespaceOrDefault( connectionRequest );
+        Map<String, String> properties = connectionRequest.getConnectionPropertiesMap();
+        if (!credentialsPresent(properties)) {
+            throw new ProtoInterfaceServiceException("No username and password given.");
+        }
+        ProtoInterfaceClient.Builder connectionBuilder = ProtoInterfaceClient.newBuilder();
+        connectionBuilder
+                .setMajorApiVersion(connectionRequest.getMajorApiVersion())
+                .setMinorApiVersion(connectionRequest.getMinorApiVersion())
+                .setClientUUID(connectionRequest.getClientUUID())
+                .setProperties(properties)
+                .setTransactionManager(transactionManager);
+
+        final CatalogUser user = authenticateUser(properties.get(DEFAULT_USERNAME_KEY), properties.get(DEFAULT_PASSWORD_KEY));
+        Transaction transaction = transactionManager.startTransaction(user, null, false, "proto-interface");
+        LogicalNamespace namespace;
+        if (connectionRequest.containsConnectionProperties("namespace")) {
+            namespace = Catalog.getInstance().getSnapshot().getNamespace(connectionRequest.getConnectionPropertiesOrThrow("namespace"));
+        } else {
+            namespace = Catalog.getInstance().getSnapshot().getNamespace(Catalog.defaultNamespaceName);
+        }
         assert namespace != null;
-        boolean isAutocommit = getAutocommitOrDefault( connectionRequest );
-        PIClient client = new PIClient(
-                connectionRequest.getClientUuid(),
-                user,
-                transactionManager,
-                namespace,
-                isAutocommit
-        );
-        openConnections.put( connectionRequest.getClientUuid(), client );
+        transaction.commit();
+        connectionBuilder
+                .setCatalogUser(user)
+                .setLogicalNamespace(namespace);
+        openConnections.put(connectionRequest.getClientUUID(), connectionBuilder.build());
         if ( log.isTraceEnabled() ) {
-            log.trace( "proto-interface established connection to user {}.", connectionRequest.getClientUuid() );
+            log.trace( "proto-interface established connection to user {}.", connectionRequest.getClientUUID());
         }
     }
 
-
-    private LogicalNamespace getNamespaceOrDefault( ConnectionRequest connectionRequest ) {
-        String namespaceName = PropertyUtils.DEFAULT_NAMESPACE_NAME;
-        if ( connectionRequest.hasConnectionProperties() && connectionRequest.getConnectionProperties().hasNamespaceName() ) {
-            namespaceName = connectionRequest.getConnectionProperties().getNamespaceName();
-        }
-        try {
-            return Catalog.getInstance().getSnapshot().getNamespace( namespaceName );
-        } catch ( Exception e ) {
-            throw new PIServiceException( "Getting namespace " + namespaceName + " failed." );
-        }
+    public ProtoInterfaceClient getClient(String clientUUID) {
+        return openConnections.get(clientUUID);
     }
 
-
-    private boolean getAutocommitOrDefault( ConnectionRequest connectionRequest ) {
-        if ( connectionRequest.hasConnectionProperties() && connectionRequest.getConnectionProperties().hasIsAutoCommit() ) {
-            return connectionRequest.getConnectionProperties().getIsAutoCommit();
-        }
-        return PropertyUtils.AUTOCOMMIT_DEFAULT;
+    private boolean credentialsPresent(Map<String, String> properties) {
+        return properties.containsKey(DEFAULT_USERNAME_KEY) && properties.containsKey(DEFAULT_PASSWORD_KEY);
     }
 
-
-    public PIClient getClient( String clientUUID ) throws PIServiceException {
-        if ( !openConnections.containsKey( clientUUID ) ) {
-            throw new PIServiceException( "Client not registered! Has the server been restarted in the meantime?" );
-        }
-        return openConnections.get( clientUUID );
+    CatalogUser authenticateUser(String username, String password) throws AuthenticationException {
+        return authenticator.authenticate(username, password);
     }
 
-
-    private TimerTask createNewCleanupTask() {
-        Runnable runnable = this::unregisterInactiveClients;
-        return new TimerTask() {
-            @Override
-            public void run() {
-                runnable.run();
-            }
-        };
+    boolean isConnected(String clientUUID) {
+        return openConnections.containsKey(clientUUID);
     }
-
-
-    private void unregisterInactiveClients() {
-        List<PIClient> inactiveClients = openConnections.values().stream()
-                .filter( c -> !c.returnAndResetIsActive() ).collect( Collectors.toList() );
-        inactiveClients.forEach( this::unregisterConnection );
-    }
-
-
-    private boolean isConnected( String clientUUID ) {
-        return openConnections.containsKey( clientUUID );
-    }
-
 }
---
+++ plugins/proto-interface/src/main/java/org/polypheny/db/protointerface/ProtoInterfaceClient.java
@@ -0,0 +1,174 @@
+/*
+ * Copyright 2019-2023 The Polypheny Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.polypheny.db.protointerface;
+
+import lombok.extern.slf4j.Slf4j;
+import org.polypheny.db.catalog.entity.CatalogDatabase;
+import org.polypheny.db.catalog.entity.CatalogUser;
+import org.polypheny.db.catalog.entity.logical.LogicalNamespace;
+import org.polypheny.db.transaction.Transaction;
+import org.polypheny.db.transaction.TransactionException;
+import org.polypheny.db.transaction.TransactionManager;
+
+import java.util.Map;
+
+@Slf4j
+public class ProtoInterfaceClient {
+    private String clientUUID;
+    private CatalogUser catalogUser;
+    private CatalogDatabase catalogDatabase;
+    private LogicalNamespace logicalNamespace;
+    private Transaction currentTransaction;
+    private TransactionManager transactionManager;
+    private Map<String, String> properties;
+    private int minorApiVersion;
+    private int majorApiVersion;
+
+    private ProtoInterfaceClient(Builder connectionBuilder) {
+        this.clientUUID = connectionBuilder.clientUUID;
+        this.catalogUser = connectionBuilder.catalogUser;
+        this.logicalNamespace = connectionBuilder.logicalNamespace;
+        this.currentTransaction = connectionBuilder.currentTransaction;
+        this.transactionManager = connectionBuilder.transactionManager;
+        this.properties = connectionBuilder.properties;
+        this.majorApiVersion = connectionBuilder.majorApiVersion;
+        this.minorApiVersion = connectionBuilder.minorApiVersion;
+    }
+
+    public String getClientUUID() {
+        return clientUUID;
+    }
+
+    public synchronized Transaction getCurrentOrCreateNewTransaction() {
+        if (currentTransaction == null || !currentTransaction.isActive()) {
+            currentTransaction = transactionManager.startTransaction(catalogUser, logicalNamespace, false, "ProtoInterface");
+        }
+        return currentTransaction;
+    }
+
+    public Transaction getCurrentTransaction() {
+        return currentTransaction;
+    }
+
+    public synchronized void commitCurrentTransaction() {
+        if (log.isTraceEnabled()) {
+            log.trace("commitCurrentTransaction() for client: {}", clientUUID);
+        }
+        if (hasNoTransaction()) {
+            if (log.isTraceEnabled()) {
+                log.trace("No open transaction for client: {}", clientUUID);
+            }
+            return;
+        }
+        try {
+            currentTransaction.commit();
+        } catch (TransactionException e) {
+            throw new ProtoInterfaceServiceException("Committing current transaction failed: " + e.getLocalizedMessage());
+        } finally {
+            endCurrentTransaction();
+        }
+    }
+
+    public synchronized void rollbackCurrentTransaction() {
+        if (log.isTraceEnabled()) {
+            log.trace("commitCurrentTransaction() for client: {}", clientUUID);
+        }
+        if (hasNoTransaction()) {
+            if (log.isTraceEnabled()) {
+                log.trace("No open transaction for client: {}", clientUUID);
+            }
+            return;
+        }
+        try {
+            currentTransaction.rollback();
+        } catch (TransactionException e) {
+            throw new ProtoInterfaceServiceException("Rollback of current transaction failed: " + e.getLocalizedMessage());
+        } finally {
+            endCurrentTransaction();
+        }
+    }
+
+    private synchronized void endCurrentTransaction() {
+        currentTransaction = null;
+    }
+
+    public boolean hasNoTransaction() {
+        return currentTransaction == null || !currentTransaction.isActive();
+    }
+
+    public static Builder newBuilder() {
+        return new Builder();
+    }
+
+    static class Builder {
+        private String clientUUID;
+        private CatalogUser catalogUser;
+        private LogicalNamespace logicalNamespace;
+        private Transaction currentTransaction;
+        private TransactionManager transactionManager;
+        private Map<String, String> properties;
+        private int minorApiVersion;
+        private int majorApiVersion;
+
+        private Builder() {
+        }
+
+        public Builder setClientUUID(String clientUUID) {
+            this.clientUUID = clientUUID;
+            return this;
+        }
+
+        public Builder setCatalogUser(CatalogUser catalogUser) {
+            this.catalogUser = catalogUser;
+            return this;
+        }
+
+        public Builder setLogicalNamespace(LogicalNamespace logicalNamespace) {
+            this.logicalNamespace = logicalNamespace;
+            return this;
+        }
+
+        public Builder setCurrentTransaction(Transaction currentTransaction) {
+            this.currentTransaction = currentTransaction;
+            return this;
+        }
+
+        public Builder setTransactionManager(TransactionManager transactionManager) {
+            this.transactionManager = transactionManager;
+            return this;
+        }
+
+        public Builder setProperties(Map<String, String> properties) {
+            this.properties = properties;
+            return this;
+        }
+
+        public Builder setMajorApiVersion(int majorApiVersion) {
+            this.majorApiVersion = majorApiVersion;
+            return this;
+        }
+
+        public Builder setMinorApiVersion(int minorApiVersion) {
+            this.minorApiVersion = minorApiVersion;
+            return this;
+        }
+
+        public ProtoInterfaceClient build() {
+            return new ProtoInterfaceClient(this);
+        }
+    }
+}
