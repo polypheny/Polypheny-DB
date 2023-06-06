0e3e813b2008c6ca77cd98215e5c1edc73a795e1
datomo <dave.lengw@gmail.com>
2023-06-06 09:33:08 +0200

    added missing handling of polyvalues in jdbc converter and dbms meta

--- plugins/jdbc-adapter-framework/src/main/java/org/polypheny/db/adapter/jdbc/JdbcToEnumerableConverter.java
+++ plugins/jdbc-adapter-framework/src/main/java/org/polypheny/db/adapter/jdbc/JdbcToEnumerableConverter.java
@@ -38,6 +38,8 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Time;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
