1589d3a2f9e1b74288d4f46755f265f878b97df4
datomo <dave.lengw@gmail.com>
2023-06-06 09:33:08 +0200

    added missing handling of polyvalues in jdbc converter and dbms meta

--- core/src/main/java/org/polypheny/db/type/entity/temporal/PolyTime.java
+++ core/src/main/java/org/polypheny/db/type/entity/temporal/PolyTime.java
@@ -91,6 +91,11 @@ public class PolyTime extends PolyTemporal {
     }
 
 
+    public static PolyTime of( Time value ) {
+        return new PolyTime( value.getTime(), TimeUnit.MILLISECOND );
+    }
+
+
     public Time asSqlTime() {
         return new Time( ofDay );
     }
