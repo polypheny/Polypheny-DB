d1550a35e6ddc1982089b04d789059486db58286
Tobias Hafner <mail@tobiashafner.ch>
2023-06-15 12:29:14 +0200

    refactor relational proto utils

--- plugins/proto-interface/src/main/java/org/polypheny/db/protointerface/utils/ProtoUtils.java
+++ plugins/proto-interface/src/main/java/org/polypheny/db/protointerface/utils/ProtoUtils.java
@@ -16,66 +16,11 @@
 
 package org.polypheny.db.protointerface.utils;
 
-import java.sql.DatabaseMetaData;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
-import org.apache.calcite.avatica.ColumnMetaData;
-import org.apache.calcite.linq4j.Ord;
-import org.apache.commons.lang3.NotImplementedException;
-import org.polypheny.db.PolyImplementation;
-import org.polypheny.db.adapter.java.JavaTypeFactory;
-import org.polypheny.db.algebra.constant.Kind;
-import org.polypheny.db.algebra.type.AlgDataType;
-import org.polypheny.db.algebra.type.AlgDataTypeField;
-import org.polypheny.db.plan.AlgOptUtil;
-import org.polypheny.db.processing.QueryProcessorHelpers;
-import org.polypheny.db.protointerface.proto.ArrayMeta;
-import org.polypheny.db.protointerface.proto.ColumnMeta;
-import org.polypheny.db.protointerface.proto.FieldMeta;
-import org.polypheny.db.protointerface.proto.Frame;
-import org.polypheny.db.protointerface.proto.ProtoValueType;
-import org.polypheny.db.protointerface.proto.Row;
 import org.polypheny.db.protointerface.proto.StatementResult;
 import org.polypheny.db.protointerface.proto.StatementStatus;
-import org.polypheny.db.protointerface.proto.StructMeta;
-import org.polypheny.db.protointerface.proto.TypeMeta;
 import org.polypheny.db.protointerface.statements.ProtoInterfaceStatement;
-import org.polypheny.db.type.PolyType;
-import org.polypheny.db.type.entity.PolyValue;
 
 public class ProtoUtils {
-
-
-
-
-    public static Row serializeToRow( List<PolyValue> row ) {
-        return Row.newBuilder()
-                .addAllValues( row.stream().map( PolyValueSerializer::serialize ).collect( Collectors.toList() ) )
-                .build();
-    }
-
-
-    public static Frame buildFrame( List<List<PolyValue>> rows, List<ColumnMeta> metas ) {
-        return Frame.newBuilder()
-                .addAllColumnMeta( metas )
-                .addAllRows( rows.stream().map( ProtoUtils::serializeToRow ).collect( Collectors.toList() ) )
-                .build();
-    }
-
-
-    public static List<ColumnMeta> buildColumnMetasFromAvatica( List<ColumnMetaData> avaticaColumnMetas ) {
-        return avaticaColumnMetas.stream().map( ProtoUtils::buildColumnMetaFromAvatica ).collect( Collectors.toList() );
-    }
-
-
-    public static ColumnMeta buildColumnMetaFromAvatica( ColumnMetaData avaticaColumnMeta ) {
-        throw new NotImplementedException("DONT USE THIS!");
-    }
-
-
-
 
 
     public static StatementStatus createStatus( ProtoInterfaceStatement protoInterfaceStatement ) {
