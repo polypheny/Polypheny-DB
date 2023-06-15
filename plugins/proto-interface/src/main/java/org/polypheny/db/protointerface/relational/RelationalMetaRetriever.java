92b5fe9550dd9981eea4bc8504af09d1d4c5f95b
Tobias Hafner <mail@tobiashafner.ch>
2023-06-15 12:29:14 +0200

    refactor relational proto utils

--- plugins/proto-interface/src/main/java/org/polypheny/db/protointerface/relational/RelationalMetaRetriever.java
+++ plugins/proto-interface/src/main/java/org/polypheny/db/protointerface/relational/RelationalMetaRetriever.java
@@ -16,11 +16,9 @@
 
 package org.polypheny.db.protointerface.relational;
 
-import com.google.common.collect.ImmutableBiMap;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.stream.Collectors;
 import org.apache.calcite.linq4j.Ord;
 import org.polypheny.db.PolyImplementation;
@@ -33,55 +31,24 @@ import org.polypheny.db.processing.QueryProcessorHelpers;
 import org.polypheny.db.protointerface.proto.ArrayMeta;
 import org.polypheny.db.protointerface.proto.ColumnMeta;
 import org.polypheny.db.protointerface.proto.FieldMeta;
-import org.polypheny.db.protointerface.proto.ParameterMeta;
-import org.polypheny.db.protointerface.proto.ProtoValue;
-import org.polypheny.db.protointerface.proto.ProtoValue.ProtoValueType;
+import org.polypheny.db.protointerface.proto.ProtoValueType;
 import org.polypheny.db.protointerface.proto.StructMeta;
 import org.polypheny.db.protointerface.proto.TypeMeta;
 import org.polypheny.db.type.PolyType;
-import org.polypheny.db.type.entity.PolyValue;
 
 public class RelationalMetaRetriever {
 
     private static final String PROTO_VALUE_TYPE_PREFIX = "PROTO_VALUE_TYPE_";
-    private static final int ORIGIN_COLUMN_INDEX = 3;
-    private static final int ORIGIN_TABLE_INDEX = 2;
-    private static final int ORIGIN_SCHEMA_INDEX = 1;
-    private static final int ORIGIN_DATABASE_INDEX = 0;
+    private static final int ORIGIN_COLUMN_NAME_OFFSET = 0;
+    private static final int ORIGIN_TABLE_NAME_OFFSET = 1;
+    private static final int ORIGIN_SCHEMA_NAME_OFFSET = 2;
 
 
-    public static List<ParameterMeta> retrieveParameterMetas( AlgDataType parameterRowType ) {
-        int index = 0;
-        return parameterRowType.getFieldList().stream()
-                .map( p -> retrieveParameterMeta( p, null ) )
-                .collect( Collectors.toList() );
-    }
-
-
-    public static List<ParameterMeta> retrieveParameterMetas( AlgDataType parameterRowType, ImmutableBiMap<String, Integer> namedIndexes ) {
-        int index = 0;
-        return parameterRowType.getFieldList().stream()
-                .map( p -> retrieveParameterMeta( p, namedIndexes.inverse().get( index ) ) )
-                .collect( Collectors.toList() );
-    }
-
-
-    private static ParameterMeta retrieveParameterMeta( AlgDataTypeField algDataTypeField, String parameterName ) {
-        AlgDataType algDataType = algDataTypeField.getType();
-        ParameterMeta.Builder metaBuilder = ParameterMeta.newBuilder();
-        metaBuilder.setName( algDataTypeField.getName() );
-        metaBuilder.setTypeName( algDataType.getPolyType().getTypeName() );
-        metaBuilder.setPrecision( QueryProcessorHelpers.getPrecision( algDataType ) );
-        metaBuilder.setScale( QueryProcessorHelpers.getScale( algDataType ) );
-        Optional.ofNullable( parameterName ).ifPresent( p -> metaBuilder.setParameterName( parameterName ) );
-        return metaBuilder.build();
-    }
-
-
-    public static List<ColumnMeta> retrieveColumnMetas( PolyImplementation<PolyValue> polyImplementation ) {
+    public static List<ColumnMeta> retrieveColumnMetas( PolyImplementation polyImplementation ) {
         AlgDataType algDataType = retrieveAlgDataType( polyImplementation );
-        AlgDataType whatever = QueryProcessorHelpers.makeStruct( polyImplementation.getStatement().getTransaction().getTypeFactory(), algDataType );
+        AlgDataType whatever = QueryProcessorHelpers.makeStruct( polyImplementation.getStatement().getPrepareContext().getTypeFactory(), algDataType );
         List<List<String>> origins = polyImplementation.getPreparedResult().getFieldOrigins();
+
         List<ColumnMeta> columns = new ArrayList<>();
         int index = 0;
         for ( Ord<AlgDataTypeField> pair : Ord.zip( whatever.getFieldList() ) ) {
@@ -93,8 +60,8 @@ public class RelationalMetaRetriever {
     }
 
 
-    private static ProtoValue.ProtoValueType getFromPolyType( PolyType polyType ) {
-        return ProtoValue.ProtoValueType.valueOf( polyType.getName() );
+    private static ProtoValueType getFromPolyType( PolyType polyType ) {
+        return ProtoValueType.valueOf( PROTO_VALUE_TYPE_PREFIX + polyType.getName() );
     }
 
 
@@ -104,27 +71,20 @@ public class RelationalMetaRetriever {
             AlgDataType type,
             List<String> origins ) {
         TypeMeta typeMeta = retrieveTypeMeta( type );
-        ColumnMeta.Builder columnMetaBuilder = ColumnMeta.newBuilder();
-        Optional.ofNullable( QueryProcessorHelpers.origin( origins, ORIGIN_TABLE_INDEX ) ).ifPresent( columnMetaBuilder::setEntityName );
-        Optional.ofNullable( QueryProcessorHelpers.origin( origins, ORIGIN_SCHEMA_INDEX ) ).ifPresent( columnMetaBuilder::setSchemaName );
-        return columnMetaBuilder
+        return ColumnMeta.newBuilder()
                 .setColumnIndex( index )
-                .setColumnName( fieldName ) // designated column name
-                .setColumnLabel( getColumnLabel( origins, type, fieldName ) ) // alias as specified in sql AS clause
+                .setColumnLabel( fieldName )
                 .setIsNullable( type.isNullable() )
                 .setLength( type.getPrecision() )
                 .setPrecision( QueryProcessorHelpers.getPrecision( type ) ) // <- same as type.getPrecision() but returns 0 if not applicable
+                .setColumnName( QueryProcessorHelpers.origin( origins, ORIGIN_COLUMN_NAME_OFFSET ) )
+                .setTableName( QueryProcessorHelpers.origin( origins, ORIGIN_TABLE_NAME_OFFSET ) )
+                .setSchemaName( QueryProcessorHelpers.origin( origins, ORIGIN_SCHEMA_NAME_OFFSET ) )
                 .setScale( type.getScale() )
                 .setTypeMeta( typeMeta )
                 //.setNamespace()
                 //TODO TH: find out how to get namespace form here
                 .build();
-    }
-
-
-    private static String getColumnLabel( List<String> origins, AlgDataType type, String fieldName ) {
-        String columnLabel = QueryProcessorHelpers.origin( origins, ORIGIN_COLUMN_INDEX );
-        return columnLabel == null ? fieldName : columnLabel;
     }
 
 
@@ -155,7 +115,7 @@ public class RelationalMetaRetriever {
                     .build();
             return TypeMeta.newBuilder()
                     .setArrayMeta( arrayMeta )
-                    .setProtoValueType( ProtoValueType.COLUMN_LIST )
+                    .setProtoValueType( ProtoValueType.PROTO_VALUE_TYPE_ARRAY )
                     .build();
         } else {
             PolyType polyType = algDataType.getPolyType();
@@ -167,8 +127,7 @@ public class RelationalMetaRetriever {
                         .collect( Collectors.toList() );
                 return TypeMeta.newBuilder()
                         .setStructMeta( StructMeta.newBuilder().addAllFieldMetas( fieldMetas ).build() )
-                        //.setProtoValueType( ProtoValueType.PROTO_VALUE_TYPE_STRUCTURED )
-                        //TODO TH: handle structured type meta in a useful way
+                        .setProtoValueType( ProtoValueType.PROTO_VALUE_TYPE_STRUCTURED )
                         .build();
             }
             return TypeMeta.newBuilder()
@@ -176,7 +135,6 @@ public class RelationalMetaRetriever {
                     .build();
         }
     }
-
 
     private static AlgDataType retrieveAlgDataType( PolyImplementation polyImplementation ) {
         switch ( polyImplementation.getKind() ) {
@@ -187,10 +145,10 @@ public class RelationalMetaRetriever {
                 // FIXME: getValidatedNodeType is wrong for DML
                 Kind kind = polyImplementation.getKind();
                 JavaTypeFactory typeFactory = polyImplementation.getStatement().getTransaction().getTypeFactory();
+                ;
                 return AlgOptUtil.createDmlRowType( kind, typeFactory );
             default:
                 return polyImplementation.getRowType();
         }
     }
-
 }
