75956c0ebc0fa7522ec56553144fc3c7047fd1d9
Neha Selvan <n.selvan@stud.unibas.ch>
2023-07-17 11:53:43 +0200

    added StreamMessage to module stream
    incl. mqtt implementation: MqttMessage.
    incl. Wrapper for MqttMessage: ReceivedMqttMessage and PublishingMessage.

---
+++ plugins/mqtt-stream/src/main/java/org/polypheny/db/mqtt/ReceivedMqttMessage.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2019-2023 The Polypheny Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.polypheny.db.mqtt;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.polypheny.db.catalog.Catalog.NamespaceType;
+
+public class ReceivedMqttMessage {
+    private final MqttMessage msg;
+    @Getter
+    private final String namespaceName;
+    @Getter
+    private final long namespaceId;
+    @Getter
+    private final NamespaceType namespaceType;
+    @Getter
+    private final String uniqueNameOfInterface;
+    @Getter
+    private final long databaseId;
+    @Getter
+    private final int userId;
+    @Getter @Setter
+    private long storeId;
+
+
+    public ReceivedMqttMessage( MqttMessage msg, String namespaceName, long namespaceId, NamespaceType namespaceType, long storeId, String uniqueNameOfInterface, long databaseId, int userId ) {
+        this.msg = msg;
+        this.namespaceName = namespaceName;
+        this.namespaceId = namespaceId;
+        this.namespaceType = namespaceType;
+        this.uniqueNameOfInterface = uniqueNameOfInterface;
+        this.databaseId = databaseId;
+        this.userId = userId;
+        this.storeId = storeId;
+    }
+
+    public String getTopic() {
+        return this.msg.getTopic();
+    }
+
+    public String getMessage() {
+        return this.msg.getMessage();
+    }
+
+}
