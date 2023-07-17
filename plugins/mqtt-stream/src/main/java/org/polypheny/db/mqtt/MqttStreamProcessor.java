2c0113df1c1fd32622d7db68d858afcb973aceef
Neha Selvan <n.selvan@stud.unibas.ch>
2023-07-17 13:36:37 +0200

    integrated MqttMessage objects in MqttStreamProcessor

--- plugins/mqtt-stream/src/main/java/org/polypheny/db/mqtt/MqttStreamProcessor.java
+++ plugins/mqtt-stream/src/main/java/org/polypheny/db/mqtt/MqttStreamProcessor.java
@@ -14,22 +14,24 @@
  * limitations under the License.
  */
 
-package org.polypheny.db.stream;
+package org.polypheny.db.mqtt;
 
 import java.nio.charset.Charset;
 import lombok.extern.slf4j.Slf4j;
+import org.polypheny.db.mqtt.ReceivedMqttMessage;
 
 @Slf4j
-public class MqttStreamProcessor extends StreamProcessorImpl implements StreamProcessor{
+public class MqttStreamProcessor {
+    ReceivedMqttMessage receivedMqttMessage;
 
-
-    public MqttStreamProcessor() {
+    public MqttStreamProcessor(ReceivedMqttMessage receivedMqttMessage) {
+        this.receivedMqttMessage = receivedMqttMessage;
     }
 
 
     //TODO: receive all additional info from Wrapper around MqttStream
-    public String processStream( String msg ) {
-        String info = extractInfo( msg );
+    public String processStream( ReceivedMqttMessage msg ) {
+        String info = extractInfo( msg.getMessage(), msg.getTopic() );
         if ( validateMsg( info ) ) {
             log.info( "Extracted and validated message: {}", msg);
             return info;
@@ -46,7 +48,7 @@ public class MqttStreamProcessor extends StreamProcessorImpl implements StreamPr
     }
 
 
-    private static String extractInfo( String msg ) {
+    private static String extractInfo( String msg, String topic ) {
         //TODO: extract the needed Info only -> based on topic attribut on right side!!};
         return msg;
     }
