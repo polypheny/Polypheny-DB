123607d6d08b04413640fa6aeec9d31fc97fe007
Martin Vahlensieck <martin.vahlensieck@unibas.ch>
2023-08-03 08:36:07 +0200

    Disallow underscores in adapter names
    Creating a graph namespace on an adapter with an underscore in the
    name causes problems at the moment.

--- dbms/src/main/java/org/polypheny/db/ddl/DdlManagerImpl.java
+++ dbms/src/main/java/org/polypheny/db/ddl/DdlManagerImpl.java
@@ -231,7 +231,9 @@ public class DdlManagerImpl extends DdlManager {
 
     @Override
     public void addAdapter( String uniqueName, String adapterName, AdapterType adapterType, Map<String, String> config ) {
-        if ( !uniqueName.matches( "^[a-z_][a-z0-9_]*$" ) ) {
+        // TODO: re-enable underscores when graph namespaces work with it
+        //if ( !uniqueName.matches( "^[a-z_][a-z0-9_]*$" ) ) {
+        if ( !uniqueName.matches( "^[a-z][a-z0-9]*$" ) ) {
             throw new RuntimeException( "Invalid unique name: unique name must only contain lower case letters, digits and underscores" );
         }
         Adapter adapter = AdapterManager.getInstance().addAdapter( adapterName, uniqueName, adapterType, config );
