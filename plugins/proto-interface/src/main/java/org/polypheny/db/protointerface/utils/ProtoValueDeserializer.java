b25dac767efd8a5362d7d53615f297f805189ea5
Tobias Hafner <t.hafner@stud.unibas.ch>
2023-09-01 09:40:29 +0200

    code cleanup

--- plugins/proto-interface/src/main/java/org/polypheny/db/protointerface/utils/ProtoValueDeserializer.java
+++ plugins/proto-interface/src/main/java/org/polypheny/db/protointerface/utils/ProtoValueDeserializer.java
@@ -16,167 +16,170 @@
 
 package org.polypheny.db.protointerface.utils;
 
-import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.math.MathContext;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 import org.apache.calcite.avatica.util.TimeUnit;
 import org.polypheny.db.protointerface.proto.IndexedParameters;
 import org.polypheny.db.protointerface.proto.ProtoBigDecimal;
 import org.polypheny.db.protointerface.proto.ProtoTime;
 import org.polypheny.db.protointerface.proto.ProtoValue;
-import org.polypheny.db.type.entity.PolyBigDecimal;
-import org.polypheny.db.type.entity.PolyBinary;
-import org.polypheny.db.type.entity.PolyBoolean;
-import org.polypheny.db.type.entity.PolyDate;
-import org.polypheny.db.type.entity.PolyDouble;
-import org.polypheny.db.type.entity.PolyFloat;
-import org.polypheny.db.type.entity.PolyInteger;
-import org.polypheny.db.type.entity.PolyList;
-import org.polypheny.db.type.entity.PolyLong;
-import org.polypheny.db.type.entity.PolyNull;
-import org.polypheny.db.type.entity.PolyString;
-import org.polypheny.db.type.entity.PolyTime;
-import org.polypheny.db.type.entity.PolyTimeStamp;
-import org.polypheny.db.type.entity.PolyValue;
+import org.polypheny.db.type.entity.*;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.MathContext;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ProtoValueDeserializer {
 
-    public static List<List<PolyValue>> deserializeParameterLists( List<IndexedParameters> parameterListsList ) {
-        return parameterListsList.stream()
-                .map( parameterList -> deserializeParameterList( parameterList.getParametersList() ) )
-                .collect( Collectors.toList() );
+    public static List<List<PolyValue>> deserializeParameterLists(List<IndexedParameters> parameterListsList) {
+        return transpose(parameterListsList.stream()
+                .map(parameterList -> deserializeParameterList(parameterList.getParametersList()))
+                .collect(Collectors.toList()));
     }
 
 
-    public static List<PolyValue> deserializeParameterList( List<ProtoValue> valuesList ) {
-        return valuesList.stream().map( ProtoValueDeserializer::deserializeProtoValue ).collect( Collectors.toList() );
+    private static List<List<PolyValue>> transpose(List<List<PolyValue>> values) {
+        int cols = values.get(0).size();
+        List<List<PolyValue>> transposed = new ArrayList<>();
+        for (int i = 0; i < cols; i++) {
+            List<PolyValue> newRow = new ArrayList<>();
+            for (List<PolyValue> value : values) {
+                newRow.add(value.get(i));
+            }
+            transposed.add(newRow);
+        }
+        return transposed;
     }
 
 
-    public static Map<String, PolyValue> deserilaizeParameterMap( Map<String, ProtoValue> valueMap ) {
+    public static List<PolyValue> deserializeParameterList(List<ProtoValue> valuesList) {
+        return valuesList.stream().map(ProtoValueDeserializer::deserializeProtoValue).collect(Collectors.toList());
+    }
+
+
+    public static Map<String, PolyValue> deserilaizeParameterMap(Map<String, ProtoValue> valueMap) {
         Map<String, PolyValue> deserializedValues = new HashMap<>();
-        valueMap.forEach( ( name, value ) -> deserializedValues.put( name, deserializeProtoValue( value ) ) );
+        valueMap.forEach((name, value) -> deserializedValues.put(name, deserializeProtoValue(value)));
         return deserializedValues;
     }
 
 
-    public static PolyValue deserializeProtoValue( ProtoValue protoValue ) {
-        switch ( protoValue.getValueCase() ) {
+    public static PolyValue deserializeProtoValue(ProtoValue protoValue) {
+        switch (protoValue.getValueCase()) {
             case BOOLEAN:
-                return deserializeToPolyBoolean( protoValue );
+                return deserializeToPolyBoolean(protoValue);
             case INTEGER:
-                return deserializeToPolyInteger( protoValue );
+                return deserializeToPolyInteger(protoValue);
             case LONG:
-                return deserializeToPolyLong( protoValue );
+                return deserializeToPolyLong(protoValue);
             case BINARY:
-                return deserializeToPolyBinary( protoValue );
+                return deserializeToPolyBinary(protoValue);
             case DATE:
-                return deserializeToPolyDate( protoValue );
+                return deserializeToPolyDate(protoValue);
             case DOUBLE:
-                return deserializeToPolyDouble( protoValue );
+                return deserializeToPolyDouble(protoValue);
             case FLOAT:
-                return deserializeToPolyFloat( protoValue );
+                return deserializeToPolyFloat(protoValue);
             case STRING:
-                return deserializeToPolyString( protoValue );
+                return deserializeToPolyString(protoValue);
             case TIME:
-                return deserializeToPolyTime( protoValue );
+                return deserializeToPolyTime(protoValue);
             case TIME_STAMP:
-                return deserializeToPolyTimeStamp( protoValue );
+                return deserializeToPolyTimeStamp(protoValue);
             case NULL:
-                return deserializeToPolyNull( protoValue );
+                return deserializeToPolyNull(protoValue);
             case BIG_DECIMAL:
-                return deserializeToPolyBigDecimal( protoValue );
+                return deserializeToPolyBigDecimal(protoValue);
             case LIST:
                 return deserializeToPolyList(protoValue);
         }
-        throw new RuntimeException( "Should never be thrown" );
+        throw new RuntimeException("Should never be thrown");
     }
 
 
-    private static PolyValue deserializeToPolyList( ProtoValue protoValue ) {
+    private static PolyValue deserializeToPolyList(ProtoValue protoValue) {
         List<PolyValue> values = protoValue.getList().getValuesList().stream()
-                .map( ProtoValueDeserializer::deserializeProtoValue )
-                .collect( Collectors.toList());
-        return new PolyList<>( values );
+                .map(ProtoValueDeserializer::deserializeProtoValue)
+                .collect(Collectors.toList());
+        return new PolyList<>(values);
     }
 
 
-    private static PolyBoolean deserializeToPolyBoolean( ProtoValue protoValue ) {
-        return new PolyBoolean( protoValue.getBoolean().getBoolean() );
+    private static PolyBoolean deserializeToPolyBoolean(ProtoValue protoValue) {
+        return new PolyBoolean(protoValue.getBoolean().getBoolean());
     }
 
 
-    private static PolyInteger deserializeToPolyInteger( ProtoValue protoValue ) {
-        return new PolyInteger( protoValue.getInteger().getInteger() );
+    private static PolyInteger deserializeToPolyInteger(ProtoValue protoValue) {
+        return new PolyInteger(protoValue.getInteger().getInteger());
     }
 
 
-    private static PolyLong deserializeToPolyLong( ProtoValue protoValue ) {
-        return new PolyLong( protoValue.getLong().getLong() );
+    private static PolyLong deserializeToPolyLong(ProtoValue protoValue) {
+        return new PolyLong(protoValue.getLong().getLong());
     }
 
 
-    private static PolyBinary deserializeToPolyBinary( ProtoValue protoValue ) {
+    private static PolyBinary deserializeToPolyBinary(ProtoValue protoValue) {
         //As poly binary's constructor uses avatica's byte string, we can't call it directly.
-        return PolyBinary.of( protoValue.getBinary().getBinary().toByteArray() );
+        return PolyBinary.of(protoValue.getBinary().getBinary().toByteArray());
     }
 
 
-    private static PolyDate deserializeToPolyDate( ProtoValue protoValue ) {
-        return new PolyDate( protoValue.getDate().getDate() );
+    private static PolyDate deserializeToPolyDate(ProtoValue protoValue) {
+        return new PolyDate(protoValue.getDate().getDate());
     }
 
 
-    private static PolyDouble deserializeToPolyDouble( ProtoValue protoValue ) {
-        return new PolyDouble( protoValue.getDouble().getDouble() );
+    private static PolyDouble deserializeToPolyDouble(ProtoValue protoValue) {
+        return new PolyDouble(protoValue.getDouble().getDouble());
     }
 
 
-    private static PolyFloat deserializeToPolyFloat( ProtoValue protoValue ) {
-        return new PolyFloat( protoValue.getFloat().getFloat() );
+    private static PolyFloat deserializeToPolyFloat(ProtoValue protoValue) {
+        return new PolyFloat(protoValue.getFloat().getFloat());
     }
 
 
-    private static PolyString deserializeToPolyString( ProtoValue protoValue ) {
-        return new PolyString( protoValue.getString().getString() );
+    private static PolyString deserializeToPolyString(ProtoValue protoValue) {
+        return new PolyString(protoValue.getString().getString());
     }
 
 
-    private static PolyTime deserializeToPolyTime( ProtoValue protoValue ) {
-        TimeUnit timeUnit = getTimeUnit( protoValue.getTime().getTimeUnit() );
-        return new PolyTime( protoValue.getTime().getValue(), timeUnit );
+    private static PolyTime deserializeToPolyTime(ProtoValue protoValue) {
+        TimeUnit timeUnit = getTimeUnit(protoValue.getTime().getTimeUnit());
+        return new PolyTime(protoValue.getTime().getValue(), timeUnit);
     }
 
 
-    private static PolyTimeStamp deserializeToPolyTimeStamp( ProtoValue protoValue ) {
-        return new PolyTimeStamp( protoValue.getTimeStamp().getTimeStamp() );
+    private static PolyTimeStamp deserializeToPolyTimeStamp(ProtoValue protoValue) {
+        return new PolyTimeStamp(protoValue.getTimeStamp().getTimeStamp());
     }
 
 
-    private static PolyNull deserializeToPolyNull( ProtoValue protoValue ) {
+    private static PolyNull deserializeToPolyNull(ProtoValue protoValue) {
         return PolyNull.NULL;
     }
 
 
-    private static PolyBigDecimal deserializeToPolyBigDecimal( ProtoValue protoValue ) {
-        return new PolyBigDecimal( deserializeToBigDecimal( protoValue ) );
+    private static PolyBigDecimal deserializeToPolyBigDecimal(ProtoValue protoValue) {
+        return new PolyBigDecimal(deserializeToBigDecimal(protoValue));
     }
 
 
-    private static BigDecimal deserializeToBigDecimal( ProtoValue protoValue ) {
+    private static BigDecimal deserializeToBigDecimal(ProtoValue protoValue) {
         ProtoBigDecimal protoBigDecimal = protoValue.getBigDecimal();
-        MathContext context = new MathContext( protoBigDecimal.getPrecision() );
+        MathContext context = new MathContext(protoBigDecimal.getPrecision());
         byte[] unscaledValue = protoBigDecimal.getUnscaledValue().toByteArray();
-        return new BigDecimal( new BigInteger( unscaledValue ), protoBigDecimal.getScale(), context );
+        return new BigDecimal(new BigInteger(unscaledValue), protoBigDecimal.getScale(), context);
     }
 
 
-    private static TimeUnit getTimeUnit( ProtoTime.TimeUnit timeUnit ) {
-        return TimeUnit.valueOf( timeUnit.name() );
+    private static TimeUnit getTimeUnit(ProtoTime.TimeUnit timeUnit) {
+        return TimeUnit.valueOf(timeUnit.name());
     }
 
 }
