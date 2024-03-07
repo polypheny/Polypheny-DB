79089f0fa6c065d42fb308116a4be98098cbcc15
Martin Vahlensieck <martin.vahlensieck@unibas.ch>
2024-03-07 17:18:27 +0100

    Add names for threads and remove shutdown hook

--- plugins/proto-interface/src/main/java/org/polypheny/db/protointerface/PIServer.java
+++ plugins/proto-interface/src/main/java/org/polypheny/db/protointerface/PIServer.java
@@ -35,29 +35,26 @@ public class PIServer {
 
 
     public PIServer( ClientManager clientManager ) throws IOException {
-        startServer( port, clientManager, PlainTransport::accept );
-
-        // used to handle unexpected shutdown of the JVM
-        Runtime.getRuntime().addShutdownHook( getShutdownHook() );
+        startServer( port, clientManager, "Plain", PlainTransport::accept );
     }
 
 
-    void startServer( int port, ClientManager clientManager, Function<Socket, Transport> createConnection ) throws IOException {
+    void startServer( int port, ClientManager clientManager, String name, Function<Socket, Transport> createConnection ) throws IOException {
         ServerSocket server = new ServerSocket( port, 16, InetAddress.getLoopbackAddress() );
         if ( log.isTraceEnabled() ) {
             log.trace( "proto-interface server started on port {}", port );
         }
-        Thread acceptor = new Thread( () -> acceptLoop( server, clientManager, createConnection ) );
+        Thread acceptor = new Thread( () -> acceptLoop( server, clientManager, name, createConnection ), "ProtoInterface" + name + "Server" );
         acceptor.start();
         servers.add( server );
     }
 
 
-    void acceptLoop( ServerSocket server, ClientManager clientManager, Function<Socket, Transport> createConnection ) {
+    void acceptLoop( ServerSocket server, ClientManager clientManager, String name, Function<Socket, Transport> createConnection ) {
         while ( true ) {
             try {
                 Socket s = server.accept();
-                Thread t = new Thread( () -> PIService.acceptConnection( createConnection.apply( s ), clientManager ) );
+                Thread t = new Thread( () -> PIService.acceptConnection( createConnection.apply( s ), clientManager ), "ProtoInterface" + name + "ClientConnection" );
                 t.start();
             } catch ( IOException e ) {
                 log.error( e.getMessage() );
@@ -72,24 +69,10 @@ public class PIServer {
 
 
     public void shutdown() throws InterruptedException, IOException {
-        servers.forEach( Util::closeNoThrow );
         if ( log.isTraceEnabled() ) {
             log.trace( "proto-interface server shutdown requested" );
         }
-    }
-
-
-    private Thread getShutdownHook() {
-        return new Thread( () -> {
-            // Use stderr here since the logger may have been reset by its JVM shutdown hook.
-            System.err.println( "shutting down proto interface since JVM is shutting down" );
-            try {
-                shutdown();
-            } catch ( IOException | InterruptedException e ) {
-                e.printStackTrace( System.err );
-            }
-            System.err.println( "server shut down" );
-        } );
+        servers.forEach( Util::closeNoThrow );
     }
 
 }
