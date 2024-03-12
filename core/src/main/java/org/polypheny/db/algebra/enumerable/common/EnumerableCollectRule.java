24977b2021c543f3ca9726245ea05bead5b9c11e
datomo <dave.lengw@gmail.com>
2024-03-12 20:11:42 +0100

    adjusted licensee header year

--- core/src/main/java/org/polypheny/db/algebra/enumerable/common/EnumerableCollectRule.java
+++ core/src/main/java/org/polypheny/db/algebra/enumerable/common/EnumerableCollectRule.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2023 The Polypheny Project
+ * Copyright 2019-2024 The Polypheny Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,7 +21,6 @@ import org.polypheny.db.algebra.AlgNode;
 import org.polypheny.db.algebra.convert.ConverterRule;
 import org.polypheny.db.algebra.core.Collect;
 import org.polypheny.db.algebra.enumerable.EnumerableConvention;
-import org.polypheny.db.algebra.enumerable.common.EnumerableCollect;
 import org.polypheny.db.plan.AlgTraitSet;
 import org.polypheny.db.plan.Convention;
 
