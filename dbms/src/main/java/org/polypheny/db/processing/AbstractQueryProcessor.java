50dda3960ec90a407c5cd4a4930ac19c5b1d46c2
datomo <dave.lengw@gmail.com>
2024-04-19 10:32:29 +0200

    handled todos, re-added missing final in transaction

--- dbms/src/main/java/org/polypheny/db/processing/AbstractQueryProcessor.java
+++ dbms/src/main/java/org/polypheny/db/processing/AbstractQueryProcessor.java
@@ -1068,8 +1068,6 @@ public abstract class AbstractQueryProcessor implements QueryProcessor, Executio
         }
 
         final AlgDataType jdbcType = QueryProcessorHelpers.makeStruct( root.alg.getCluster().getTypeFactory(), root.validatedRowType );
-        //TODO DL: get metadata instead of null
-        List<List<String>> fieldOrigins = Collections.nCopies( jdbcType.getFieldCount(), null );
 
         final Prefer prefer = Prefer.ARRAY;
         final Convention resultConvention =
@@ -1115,7 +1113,7 @@ public abstract class AbstractQueryProcessor implements QueryProcessor, Executio
         return new PreparedResultImpl<>(
                 resultType,
                 parameterRowType,
-                fieldOrigins,
+                Collections.nCopies( jdbcType.getFieldCount(), null ), // we do not have the origin of the fields so we provide null for all
                 root.collation.getFieldCollations().isEmpty()
                         ? ImmutableList.of()
                         : ImmutableList.of( root.collation ),
