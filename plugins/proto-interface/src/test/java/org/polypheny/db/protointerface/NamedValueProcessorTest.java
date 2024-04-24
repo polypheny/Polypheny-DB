7ed34c81bdab8de57a966979a6e5864c2908e200
Tobias Hafner <mail@tobiashafner.ch>
2024-04-24 13:37:40 +0200

    Complete named value processor tests

--- plugins/proto-interface/src/test/java/org/polypheny/db/protointerface/NamedValueProcessorTest.java
+++ plugins/proto-interface/src/test/java/org/polypheny/db/protointerface/NamedValueProcessorTest.java
@@ -16,16 +16,27 @@
 
 package org.polypheny.db.protointerface;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.polypheny.db.TestHelper;
 import org.polypheny.db.type.entity.PolyString;
 import org.polypheny.db.type.entity.PolyValue;
 
 
 public class NamedValueProcessorTest {
+
+    @BeforeAll
+    public static void init() {
+        // needed to launch polypheny
+        TestHelper.getInstance();
+    }
+
 
     @Test
     public void replacePlaceholders__missingValue() {
@@ -37,6 +48,26 @@ public class NamedValueProcessorTest {
             values.put( "last_name", PolyString.of( "hafner" ) );
 
             NamedValueProcessor namedValueProcessor = new NamedValueProcessor( statement );
+            List<PolyValue> parameters = namedValueProcessor.transformValueMap( values );
+        } );
+    }
+
+
+    @Test
+    public void replacePlaceholders__checkValue() {
+        assertThrows( PIServiceException.class, () -> {
+            final String statement = "select * from people where (first_name = :first_name or last_name= :last_name) and project = :project);";
+
+            final Map<String, PolyValue> values = new HashMap<>();
+            values.put( "first_name", PolyString.of( "tobias" ) );
+            values.put( "last_name", PolyString.of( "hafner" ) );
+            values.put( "project", PolyString.of( "polypheny" ) );
+
+            NamedValueProcessor namedValueProcessor = new NamedValueProcessor( statement );
+            List<PolyValue> parameters = namedValueProcessor.transformValueMap( values );
+            assertEquals( "tobias", parameters.get( 0 ).asString().getValue() );
+            assertEquals( "hafner", parameters.get( 1 ).asString().getValue() );
+            assertEquals( "polypheny", parameters.get( 2 ).asString().getValue() );
         } );
     }
 
