585dbd6832cb62854711e57c659e446d2629aece
Tobias Hafner <mail@tobiashafner.ch>
2024-05-16 21:28:03 +0200

    Add graph results

--- plugins/prism-interface/src/main/java/org/polypheny/db/prisminterface/utils/PrismUtils.java
+++ plugins/prism-interface/src/main/java/org/polypheny/db/prisminterface/utils/PrismUtils.java
@@ -17,16 +17,22 @@
 package org.polypheny.db.prisminterface.utils;
 
 import java.util.List;
-import org.polypheny.db.catalog.exceptions.GenericRuntimeException;
 import org.polypheny.db.prisminterface.statements.PIPreparedStatement;
 import org.polypheny.db.prisminterface.statements.PIStatement;
 import org.polypheny.db.type.PolyType;
 import org.polypheny.db.type.entity.PolyValue;
+import org.polypheny.db.type.entity.graph.PolyEdge;
+import org.polypheny.db.type.entity.graph.PolyNode;
+import org.polypheny.db.type.entity.graph.PolyPath;
 import org.polypheny.prism.ColumnMeta;
 import org.polypheny.prism.DocumentFrame;
 import org.polypheny.prism.Frame;
+import org.polypheny.prism.GraphFrame;
 import org.polypheny.prism.PreparedStatementSignature;
 import org.polypheny.prism.ProtoDocument;
+import org.polypheny.prism.ProtoEdge;
+import org.polypheny.prism.ProtoNode;
+import org.polypheny.prism.ProtoPath;
 import org.polypheny.prism.ProtoPolyType;
 import org.polypheny.prism.RelationalFrame;
 import org.polypheny.prism.Row;
@@ -87,6 +93,21 @@ public class PrismUtils {
     }
 
 
+    private static List<ProtoNode> serializeToNodes( List<List<PolyValue>> data ) {
+        return data.stream().map( e -> PolyValueSerializer.buildProtoNode( (PolyNode) (e.get( 0 )) ) ).collect( Collectors.toList() );
+    }
+
+
+    private static List<ProtoEdge> serializeToEdges( List<List<PolyValue>> data ) {
+        return data.stream().map( e -> PolyValueSerializer.buildProtoEdge( (PolyEdge) (e.get( 0 )) ) ).collect( Collectors.toList() );
+    }
+
+
+    private static List<ProtoPath> serilaizeToPaths( List<List<PolyValue>> data ) {
+        return data.stream().map( e -> PolyValueSerializer.buildProtoPath( (PolyPath) (e.get( 0 )) ) ).collect( Collectors.toList() );
+    }
+
+
     public static Frame buildRelationalFrame( boolean isLast, List<List<PolyValue>> rows, List<ColumnMeta> metas ) {
         RelationalFrame relationalFrame = RelationalFrame.newBuilder()
                 .addAllColumnMeta( metas )
@@ -114,9 +135,22 @@ public class PrismUtils {
     }
 
 
-    public static Frame buildGraphFrame(boolean isLast, List<List<PolyValue>> data) {
-        throw new GenericRuntimeException( "Feature not implemented" );
+    public static Frame buildGraphFrame( boolean isLast, List<List<PolyValue>> data ) {
+        GraphFrame.Builder graphFrameBuilder = GraphFrame.newBuilder();
+        PolyType elementType = data.get( 0 ).get( 0 ).getType();
+        switch ( elementType ) {
+            case NODE -> graphFrameBuilder.addAllNodes( serializeToNodes( data ) );
+            case EDGE -> graphFrameBuilder.addAllEdges( serializeToEdges( data ) );
+            case PATH -> graphFrameBuilder.addAllPaths( serilaizeToPaths( data ) );
+            default -> throw new RuntimeException( "Should never be thrown!" );
+        }
+
+        return Frame.newBuilder()
+                .setIsLast( isLast )
+                .setGraphFrame( graphFrameBuilder.build() )
+                .build();
     }
+
 
     public static ProtoPolyType getProtoFromPolyType( PolyType polyType ) {
         return ProtoPolyType.valueOf( polyType.getName() );
