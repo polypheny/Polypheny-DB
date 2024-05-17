5cc1df79ca39b46606f15f7026624480bc3ba18e
Tobias Hafner <mail@tobiashafner.ch>
2024-05-17 16:26:42 +0200

    Add cypher tests

--- plugins/prism-interface/src/main/java/org/polypheny/db/prisminterface/statementProcessing/StatementProcessor.java
+++ plugins/prism-interface/src/main/java/org/polypheny/db/prisminterface/statementProcessing/StatementProcessor.java
@@ -64,7 +64,7 @@ public class StatementProcessor {
                 .query( piStatement.getQuery() )
                 .language( piStatement.getLanguage() )
                 .namespaceId( piStatement.getNamespace().id )
-                .transactionManager( piStatement.getTransaction().getTransactionManager() )
+                .transactions( List.of(piStatement.getTransaction()) )
                 .origin( ORIGIN )
                 .build();
         List<ImplementationContext> implementations = LanguageManager.getINSTANCE().anyPrepareQuery( context, statement );
