7fe90f8eb313df937c45c07d071134abe8508086
Tobias Hafner <mail@tobiashafner.ch>
2024-07-05 10:09:40 +0200

    Abstract plain transport

--- plugins/prism-interface/src/main/java/org/polypheny/db/prisminterface/transport/PlainTransport.java
+++ plugins/prism-interface/src/main/java/org/polypheny/db/prisminterface/transport/PlainTransport.java
@@ -1,47 +1,26 @@
-/*
- * Copyright 2019-2024 The Polypheny Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.polypheny.db.prisminterface.transport;
 
-import java.io.EOFException;
 import java.io.IOException;
-import java.net.StandardSocketOptions;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.Optional;
-import org.polypheny.db.catalog.exceptions.GenericRuntimeException;
-import org.polypheny.db.util.Util;
 
-public class PlainTransport implements Transport {
+public class PlainTransport extends SocketTransport {
 
     private final static String VERSION = "plain-v1@polypheny.com";
 
-    protected final SocketChannel con;
-
 
     PlainTransport( SocketChannel con ) throws IOException {
-        this( con, VERSION );
+        super( con );
+        exchangeVersion( VERSION );
     }
 
 
     protected PlainTransport( SocketChannel con, String version ) throws IOException {
-        this.con = con;
+        super( con );
         exchangeVersion( version );
     }
 
@@ -78,23 +57,12 @@ public class PlainTransport implements Transport {
         ByteBuffer length = bb.slice( 0, 1 );
         bb.position( 1 );
         readEntireBuffer( length );
-        if ( length.get() != len ) {
+        byte i_length = length.get();
+        if ( i_length != len ) {
             throw new IOException( "Invalid version response length" );
         }
         readEntireBuffer( bb );
         return bb.array();
-    }
-
-
-    private void writeEntireBuffer( ByteBuffer bb ) throws IOException {
-        synchronized ( con ) {
-            while ( bb.remaining() > 0 ) {
-                int i = con.write( bb );
-                if ( i == -1 ) {
-                    throw new EOFException();
-                }
-            }
-        }
     }
 
 
@@ -106,17 +74,6 @@ public class PlainTransport implements Transport {
         bb.put( msg );
         bb.rewind();
         writeEntireBuffer( bb );
-    }
-
-
-    private void readEntireBuffer( ByteBuffer bb ) throws IOException {
-        while ( bb.remaining() > 0 ) {
-            int i = con.read( bb );
-            if ( i == -1 ) {
-                throw new EOFException();
-            }
-        }
-        bb.rewind();
     }
 
 
@@ -132,23 +89,6 @@ public class PlainTransport implements Transport {
         bb = ByteBuffer.allocate( (int) length );
         readEntireBuffer( bb );
         return bb.array();
-    }
-
-
-    @Override
-    public void close() {
-        Util.closeNoThrow( con );
-    }
-
-
-    public static Transport accept( SocketChannel con ) {
-        try {
-            con.setOption( StandardSocketOptions.TCP_NODELAY, true );
-            return new PlainTransport( con );
-        } catch ( IOException e ) {
-            Util.closeNoThrow( con );
-            throw new GenericRuntimeException( e );
-        }
     }
 
 }
