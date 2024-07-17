e53caae4d7ba7a3f2c4968201419e6b72e24e424
Tobias Hafner <mail@tobiashafner.ch>
2024-07-17 20:30:27 +0200

    Add error on duplicate files

--- plugins/csv-adapter/src/main/java/org/polypheny/db/adapter/csv/CsvSchema.java
+++ plugins/csv-adapter/src/main/java/org/polypheny/db/adapter/csv/CsvSchema.java
@@ -90,7 +90,16 @@ public class CsvSchema extends Namespace {
         List<Integer> fieldIds = new ArrayList<>();
 
         List<ExportedColumn> columns = csvSource.getExportedColumns().get( table.name );
+        if (columns == null) {
+            /*
+            TODO:   The source determines table names by file name. The DdlManager enumerates duplicates like name0, name1 and so forth.
+                    This remains unnoticed by the source. 'getExportedColumns' thus returns the columns under a wrong name. A way must be found to sync the of the logical in the
+                    DdlManager with the name in the source. Currently we just fail the creation of sources with duplicate file names.
+             */
 
+            String unenumeratedEntityName = table.name.replaceAll("\\d+$", "");
+            throw new RuntimeException("A logical relational entity with name '" + unenumeratedEntityName + "' already exists.");
+        }
         for ( PhysicalColumn column : table.getColumns() ) {
             AlgDataType sqlType = sqlType( typeFactory, column.type, column.length, column.scale, null );
             fieldInfo.add( column.id, column.name, columns.get( column.position ).physicalColumnName, sqlType ).nullable( column.nullable );
