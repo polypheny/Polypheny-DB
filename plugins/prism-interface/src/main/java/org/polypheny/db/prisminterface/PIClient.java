0a86c9d6fcca9647bbc115276af0aac63f135350
Tobias Hafner <mail@tobiashafner.ch>
2024-07-19 08:47:48 +0200

    Put classes into corresponding packages

--- plugins/prism-interface/src/main/java/org/polypheny/db/prisminterface/PIClient.java
+++ plugins/prism-interface/src/main/java/org/polypheny/db/prisminterface/PIClient.java
@@ -21,6 +21,7 @@ import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.polypheny.db.catalog.entity.LogicalUser;
 import org.polypheny.db.catalog.entity.logical.LogicalNamespace;
+import org.polypheny.db.prisminterface.metaRetrieval.PIClientInfoProperties;
 import org.polypheny.db.prisminterface.statements.StatementManager;
 import org.polypheny.db.transaction.Transaction;
 import org.polypheny.db.transaction.TransactionException;
@@ -39,7 +40,7 @@ public class PIClient {
     @Getter
     private final StatementManager statementManager;
     @Getter
-    private final PIClientInfoProperties PIClientInfoProperties;
+    private final org.polypheny.db.prisminterface.metaRetrieval.PIClientInfoProperties PIClientInfoProperties;
     @Getter
     @Setter
     private boolean isAutoCommit;
