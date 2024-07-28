8242172d9613e76199da24c961c8ce6ccbdbe614
alyaa999 <alyaamamoon999@gmail.com>
2024-07-28 22:45:49 +0300

    add new tests to Union Test file

--- dbms/src/test/java/org/polypheny/db/cypher/clause/general/UnionTest.java
+++ dbms/src/test/java/org/polypheny/db/cypher/clause/general/UnionTest.java
@@ -26,84 +26,185 @@ import java.util.Arrays;
 public class UnionTest extends CypherTestTemplate {
 
 
-
     @BeforeEach
     public void reset() {
         tearDown();
         createGraph();
     }
+    protected static final String SINGLE_NODE_MOVIE = "CREATE (wallStreet:Movie {title: 'Wall Street' , released : 2002})";
+
 
     @Test
     public void simpleUnionTest() {
         execute( SINGLE_NODE_PERSON_1 );
-        execute( SINGLE_NODE_PERSON_2);
+        execute( SINGLE_NODE_PERSON_2 );
         execute( SINGLE_NODE_PERSON_1 );
-        execute( SINGLE_NODE_PERSON_2);
-        execute(SINGLE_NODE_ANIMAL);
-        execute( SINGLE_NODE_ANIMAL);
 
-
-        GraphResult res =  execute( "MATCH (n:Actor)\n"
-                + "RETURN n.name AS name\n"
+        GraphResult res = execute( "MATCH (n:Person)\n"
+                + "RETURN n.name \n"
                 + "UNION \n"
-                + "MATCH (n:Movie)\n"
-                + "RETURN n.title AS name" );
-
-
+                + "MATCH (n:Person)\n"
+                + "RETURN n.name " );
 
         containsRows( res, true, false,
                 Row.of( TestLiteral.from( "Max" ) ),
-                Row.of( TestLiteral.from( "Hans" ) ),
-                Row.of( TestLiteral.from( "Kira" ) ));
+                Row.of( TestLiteral.from( "Hans" ) ) );
     }
+
+
+    @Test
+    public void DifferentStructureUnionTest() {
+        execute( SINGLE_NODE_PERSON_1 );
+        execute( SINGLE_NODE_PERSON_1 );
+        execute( SINGLE_NODE_MOVIE);
+
+        GraphResult res = execute( "MATCH (p:Person)\n"
+                + "RETURN p.name AS name\n"
+                + "UNION\n"
+                + "MATCH (m :Movie)\n"
+                + "RETURN p.Title AS name\n" );
+
+        assert containsRows( res, true, false,
+                Row.of( TestLiteral.from( "Max" ) ),
+                Row.of( TestLiteral.from( "Wall Street"  )) );
+    }
+
+    @Test
+    public void NullPropertiesUnionTest()
+    {
+        execute( SINGLE_NODE_PERSON_COMPLEX_1);
+        execute( SINGLE_NODE_PERSON_COMPLEX_1);
+        execute( SINGLE_NODE_MOVIE);
+
+        GraphResult res = execute( "MATCH (p:Person)\n"
+                + "RETURN p.name AS name, p.age AS age, NULL AS title, NULL AS released\n"
+                + "UNION\n"
+                + "MATCH (m:Movie)\n"
+                + "RETURN NULL AS name, NULL AS age, m.title AS title, m.released AS released\n");
+
+        assert containsRows( res, true, false,
+                Row.of( TestLiteral.from( "Ann" ) , TestLiteral.from( 45 ) , TestLiteral.from( null ),TestLiteral.from( null )),
+                Row.of( TestLiteral.from( null   ),TestLiteral.from( null  ),TestLiteral.from( "Wall Street" ), TestLiteral.from( 2002 )) );
+    }
+
+
     @Test
     public void allUnionTest() {
         execute( SINGLE_NODE_PERSON_1 );
-        execute( SINGLE_NODE_PERSON_2);
-        execute(SINGLE_NODE_ANIMAL);
-        execute( SINGLE_NODE_ANIMAL);
+        execute( SINGLE_NODE_PERSON_2 );
+        execute( SINGLE_NODE_PERSON_1);
+        execute( SINGLE_NODE_PERSON_2 );
 
-        GraphResult res = execute( "MATCH (n:Actor)\n"
-                + "RETURN n.name AS name\n"
-                + "UNION ALL\n"
-                + "MATCH (n:Movie)\n"
-                + "RETURN n.title AS name" );
-
+        GraphResult res = execute( "MATCH (n:Person)\n"
+                + "RETURN n.name \n"
+                + "UNION ALL \n"
+                + "MATCH (n:Person)\n"
+                + "RETURN n.name " );
 
         containsRows( res, true, false,
                 Row.of( TestLiteral.from( "Max" ) ),
-                Row.of( TestLiteral.from( "Hans" ) ),
-                Row.of( TestLiteral.from( "Kira" ) ),
-                Row.of( TestLiteral.from( "Kira" ) ) );
+                Row.of( TestLiteral.from( "Hans" ) ) ,
+                Row.of(TestLiteral.from( "Max" )),
+                Row.of(TestLiteral.from( "Hans" )));
 
 
     }
 
+    @Test
+    public void DifferentStructureAllUnionTest() {
+        execute( SINGLE_NODE_PERSON_1 );
+        execute( SINGLE_NODE_MOVIE);
+        execute( SINGLE_NODE_PERSON_1 );
+        execute( SINGLE_NODE_MOVIE );
 
+        GraphResult res = execute( "MATCH (p:Person)\n"
+                + "RETURN p.name AS name\n"
+                + "UNION ALL\n"
+                + "MATCH (m :Movie)\n"
+                + "RETURN p.Title AS name\n" );
+
+        assert containsRows( res, true, false,
+                Row.of( TestLiteral.from( "Max" ) ),
+                Row.of( TestLiteral.from( "Wall Street"  )),
+                Row.of( TestLiteral.from( "Max"  )),
+                Row.of( TestLiteral.from( "Wall Street"  )));
+    }
+
+    @Test
+    public void NullPropertiesAllUnionTest()
+    {
+        execute( SINGLE_NODE_PERSON_COMPLEX_1);
+        execute( SINGLE_NODE_PERSON_COMPLEX_1 );
+        execute( SINGLE_NODE_MOVIE );
+        execute( SINGLE_NODE_MOVIE);
+
+        GraphResult res = execute( "MATCH (p:Person)\n"
+                + "RETURN p.name AS name, p.age AS age, NULL AS title, NULL AS released\n"
+                + "UNION ALL\n"
+                + "MATCH (m:Movie)\n"
+                + "RETURN NULL AS name, NULL AS age, m.title AS title, m.released AS released\n");
+
+        assert containsRows( res, true, false,
+                Row.of( TestLiteral.from( "Ann" ) , TestLiteral.from( 45 ) , TestLiteral.from( null ),TestLiteral.from( null )),
+                Row.of( TestLiteral.from( "Ann" ) , TestLiteral.from( 45 ) , TestLiteral.from( null ),TestLiteral.from( null )),
+                Row.of( TestLiteral.from( null   ),TestLiteral.from( null  ),TestLiteral.from( "Wall Street" ), TestLiteral.from( 2002 )),
+                Row.of( TestLiteral.from( null   ),TestLiteral.from( null  ),TestLiteral.from( "Wall Street" ), TestLiteral.from( 2002 )) );
+    }
 
 
 
     @Test
     public void distinctUnionTest() {
         execute( SINGLE_NODE_PERSON_1 );
-        execute( SINGLE_NODE_PERSON_2);
-        execute( SINGLE_NODE_PERSON_1 );
-        execute( SINGLE_NODE_PERSON_2);
-        execute(SINGLE_NODE_ANIMAL);
-        execute( SINGLE_NODE_ANIMAL);
+        execute( SINGLE_NODE_PERSON_2 );
+        execute( SINGLE_NODE_PERSON_1);
+        execute( SINGLE_NODE_PERSON_2 );
 
-        GraphResult res = execute( "MATCH (n:Actor)\n"
-                + "RETURN n.name AS name\n"
-                + "UNION DISTINCT\n"
-                + "MATCH (n:Movie)\n"
-                + "RETURN n.title AS name" );
-
+        GraphResult res = execute( "MATCH (n:Person)\n"
+                + "RETURN n.name \n"
+                + "UNION DISTINCT  \n"
+                + "MATCH (n:Person)\n"
+                + "RETURN n.name " );
 
         containsRows( res, true, false,
                 Row.of( TestLiteral.from( "Max" ) ),
-                Row.of( TestLiteral.from( "Hans" ) ),
-                Row.of( TestLiteral.from( "Kira" ) ));
+                Row.of( TestLiteral.from( "Hans" ) ) );
+    }
 
+
+    @Test
+    public void DifferentStructureDistinctUnionTest() {
+        execute( SINGLE_NODE_PERSON_1 );
+        execute( SINGLE_NODE_PERSON_1 );
+        execute( SINGLE_NODE_MOVIE);
+
+        GraphResult res = execute( "MATCH (p:Person)\n"
+                + "RETURN p.name AS name\n"
+                + "UNION DISTINCT\n"
+                + "MATCH (m :Movie)\n"
+                + "RETURN p.Title AS name\n" );
+
+        assert containsRows( res, true, false,
+                Row.of( TestLiteral.from( "Max" ) ),
+                Row.of( TestLiteral.from( "Wall Street"  )) );
+    }
+
+    @Test
+    public void NullPropertiesDistinctUnionTest()
+    {
+        execute( SINGLE_NODE_PERSON_COMPLEX_1);
+        execute( SINGLE_NODE_PERSON_COMPLEX_1);
+        execute( SINGLE_NODE_MOVIE);
+
+        GraphResult res = execute( "MATCH (p:Person)\n"
+                + "RETURN p.name AS name, p.age AS age, NULL AS title, NULL AS released\n"
+                + "UNION DISTINCT \n"
+                + "MATCH (m:Movie)\n"
+                + "RETURN NULL AS name, NULL AS age, m.title AS title, m.released AS released\n");
+
+        assert containsRows( res, true, false,
+                Row.of( TestLiteral.from( "Ann" ) , TestLiteral.from( 45 ) , TestLiteral.from( null ),TestLiteral.from( null )),
+                Row.of( TestLiteral.from( null   ),TestLiteral.from( null  ),TestLiteral.from( "Wall Street" ), TestLiteral.from( 2002 )) );
     }
 
 }
