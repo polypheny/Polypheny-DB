8e051789c2a99282939f052d266daff0371c1a88
alyaa999 <alyaamamoon999@gmail.com>
2024-08-04 03:37:33 +0300

    Add test  coverage for Operators ( boolean - comparison - string )

---
+++ dbms/src/test/java/org/polypheny/db/cypher/Operators/BooleanOperators.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2019-2024 The Polypheny Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.polypheny.db.cypher.Operators;
+
+import net.bytebuddy.description.type.TypeList.Generic;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.polypheny.db.cypher.CypherTestTemplate;
+import org.polypheny.db.cypher.helper.TestLiteral;
+import org.polypheny.db.webui.models.results.GraphResult;
+
+public class BooleanOperators extends CypherTestTemplate {
+    @BeforeEach
+    public void setUp() {
+        tearDown();
+        createGraph();
+    }
+
+    @Test
+    public void conjunctionOperatorTest()
+    {
+        GraphResult res =  execute( "WITH true as a , false as b RETURN a AND  b " );
+        assert  containsRows( res , true , false , Row.of( TestLiteral.from( false ) ) );
+        res = execute( "WITH true as a , true  as b RETURN a AND  b " );
+        assert  containsRows( res , true , false , Row.of( TestLiteral.from(true  ) ) );
+
+    }
+
+    @Test
+    public void disjunctionOperatorTest()
+    {
+        GraphResult res =  execute( "WITH true as a , false as b RETURN a OR  b " );
+        assert  containsRows( res , true , false , Row.of( TestLiteral.from( true  ) ) );
+        res = execute( "WITH false as a , false  as b RETURN a OR b " );
+        assert  containsRows( res , true , false , Row.of( TestLiteral.from(false   ) ) );
+
+    }
+
+    @Test
+    public void  exclusiveDisjunctionOperatorTest()
+    {
+        GraphResult res =  execute( "WITH true as a , false as b RETURN a XOR b " );
+        assert  containsRows( res , true , false , Row.of( TestLiteral.from( true  ) ) );
+        res = execute( "WITH true as a , true  as b RETURN a XOR b " );
+        assert  containsRows( res , true , false , Row.of( TestLiteral.from(false  ) ) );
+        res = execute( "WITH false as a , false  as b RETURN a XOR b " );
+        assert  containsRows( res , true , false , Row.of( TestLiteral.from(true  ) ) );
+
+    }
+
+    @Test
+    public void negationOperatorTest()
+    {
+        GraphResult res = execute( "WITH true as a RETURN  NOT a  " );
+        assert  containsRows( res , true , false , Row.of( TestLiteral.from(false  ) ) );
+
+        res = execute( "WITH false as a RETURN  NOT a  " );
+        assert  containsRows( res , true , false , Row.of( TestLiteral.from(true   ) ) );
+
+    }
+
+}
---
+++ dbms/src/test/java/org/polypheny/db/cypher/Operators/ComparisonOperations.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2019-2024 The Polypheny Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.polypheny.db.cypher.Operators;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.polypheny.db.cypher.CypherTestTemplate;
+import org.polypheny.db.cypher.helper.TestLiteral;
+import org.polypheny.db.webui.models.results.GraphResult;
+
+public class ComparisonOperations extends CypherTestTemplate {
+    @BeforeEach
+    public void setUp() {
+        tearDown();
+        createGraph();
+    }
+
+
+    @Test
+    public void IsNullOperatorTest() {
+      GraphResult res  =  execute( "Return null is not null as  Result" );
+      assert  containsRows(  res ,true , false ,Row.of( TestLiteral.from( false ) ) );
+    }
+
+    @Test
+    public void IsNotNullFunction() {
+      GraphResult res  = execute( "Return null is null as  Result" );
+        assert  containsRows(  res ,true , false ,Row.of( TestLiteral.from( true  ) ) );
+
+    }
+
+    @Test
+    public void greaterThanOperatorTest()
+    {
+      GraphResult res =  execute( "Return 1 > 2 as result " );
+      assert  containsRows( res , true , false ,Row.of( TestLiteral.from( false ) ) );
+    }
+
+    @Test
+    public void smallerThanOperatorTest()
+    {
+        GraphResult res =  execute( "Return 1 < 2 as result " );
+        assert  containsRows( res , true , false ,Row.of( TestLiteral.from( true) ) );
+    }
+    @Test
+    public void greaterThanOrEqualOperatorTest()
+    {
+        GraphResult res =  execute( "Return 1 >= 2 as result " );
+        assert  containsRows( res , true , false ,Row.of( TestLiteral.from( false ) ) );
+    }
+    @Test
+    public void smallerThanOrEqualOperatorTest()
+    {
+        GraphResult res =  execute( "Return 1 <= 2 as result " );
+        assert  containsRows( res , true , false ,Row.of( TestLiteral.from( true ) ) );
+    }
+
+    @Test
+    public void equalityOperatorTest()
+    {
+        GraphResult res =  execute( "Return 2 = 2 as result " );
+        assert  containsRows( res , true , false ,Row.of( TestLiteral.from( true ) ) );
+    }
+
+    @Test
+    public void inequalityOperatorTest()
+    {
+        GraphResult res =  execute( "Return 1 <> 2 as result " );
+        assert  containsRows( res , true , false ,Row.of( TestLiteral.from( false  ) ) );
+    }
+
+}
