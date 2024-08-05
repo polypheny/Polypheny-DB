0cf738cb6c59edb688740b5403a072d2fff74ebf
alyaa999 <alyaamamoon999@gmail.com>
2024-08-06 01:24:05 +0300

    add coverage test for Point fun

---
+++ dbms/src/test/java/org/polypheny/db/cypher/Functions/SpatialFunTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2019-2024 The Polypheny Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.polypheny.db.cypher.Functions;
+
+import javassist.bytecode.CodeIterator.Gap;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.polypheny.db.cypher.CypherTestTemplate;
+import org.polypheny.db.cypher.helper.TestLiteral;
+import org.polypheny.db.webui.models.results.GraphResult;
+
+public class SpatialFunTest extends CypherTestTemplate {
+
+    @BeforeEach
+    public void reset() {
+        tearDown();
+        createGraph();
+    }
+
+
+    @Test
+    public void cartesian2DPointFunTest() {
+
+        GraphResult res = execute( "WITH point({x: 3, y: 4}) AS p\n"
+                + "RETURN\n"
+                + "  p.x AS x,\n"
+                + "  p.y AS y,\n"
+                + "  p.crs AS crs,\n"
+                + "  p.srid AS srid" );
+
+        assert containsRows( res, true, false,
+                Row.of( TestLiteral.from( 3.0 ), TestLiteral.from( 4.0 ),
+                        TestLiteral.from( "cartesian" ), TestLiteral.from( 7203 ) ) );
+    }
+
+}
