78d1470c23af08d87353d8bba6fe4bad771242c0
Tobias Hafner <mail@tobiashafner.ch>
2024-08-06 14:47:08 +0200

    Add input stream for strings

--- plugins/prism-interface/src/main/java/org/polypheny/db/prisminterface/streaming/StringPIInputStream.java
+++ plugins/prism-interface/src/main/java/org/polypheny/db/prisminterface/streaming/StringPIInputStream.java
@@ -1,41 +1,62 @@
-/*
- * Copyright 2019-2024 The Polypheny Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.polypheny.db.prisminterface.streaming;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 import org.polypheny.prism.StreamAcknowledgement;
 import org.polypheny.prism.StreamFrame;
 
-public class StringPIInputStream implements PIInputStream {
+public class StringPIInputStream extends Reader implements PIInputStream {
+
+    private final BlockingQueue<String> bufferQueue = new LinkedBlockingQueue<>();
+    private String currentBuffer = null;
+    private int currentIndex = 0;
+    private boolean isLast = false;
+    private boolean isClosed = false;
+
+
+    @Override
+    public int read( char[] cbuf, int off, int len ) throws IOException {
+        if ( currentBuffer == null || currentIndex >= currentBuffer.length() ) {
+            if ( bufferQueue.isEmpty() && isLast ) {
+                return -1;
+            }
+            try {
+                currentBuffer = bufferQueue.take();
+                currentIndex = 0;
+            } catch ( InterruptedException e ) {
+                throw new RuntimeException( e );
+            }
+        }
+
+        int charsToRead = Math.min( len, currentBuffer.length() - currentIndex );
+        currentBuffer.getChars( currentIndex, currentIndex + charsToRead, cbuf, off );
+        currentIndex += charsToRead;
+        return charsToRead;
+    }
+
+
+    public StreamAcknowledgement appendFrame( StreamFrame frame ) {
+        String data = frame.getString();
+        try {
+            bufferQueue.put( data );
+        } catch ( InterruptedException e ) {
+            throw new RuntimeException( e );
+        }
+        isLast = frame.getIsLast();
+        return StreamAcknowledgement.newBuilder().setCloseStream( isClosed ).build();
+    }
+
 
     @Override
     public void close() {
-
+        isClosed = true;
     }
 
 
-    @Override
-    public StreamAcknowledgement appendFrame( StreamFrame frame ) {
-        return StreamAcknowledgement.newBuilder().setCloseStream( false ).build();
-    }
-
-
-    @Override
     public boolean isClosed() {
-        return false;
+        return isClosed;
     }
 
 }
