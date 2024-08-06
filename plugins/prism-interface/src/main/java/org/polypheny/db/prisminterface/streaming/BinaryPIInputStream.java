81c5998d857bc01bdb9990b61466b32973a91d8c
Tobias Hafner <mail@tobiashafner.ch>
2024-08-06 14:27:45 +0200

    Improve binary input stream performance

--- plugins/prism-interface/src/main/java/org/polypheny/db/prisminterface/streaming/BinaryPIInputStream.java
+++ plugins/prism-interface/src/main/java/org/polypheny/db/prisminterface/streaming/BinaryPIInputStream.java
@@ -8,43 +8,43 @@ import org.polypheny.prism.StreamFrame;
 
 public class BinaryPIInputStream extends InputStream implements PIInputStream {
 
-    BlockingQueue<Byte> buffer = new LinkedBlockingQueue<>();
+    BlockingQueue<byte[]> bufferQueue = new LinkedBlockingQueue<>();
+    byte[] currentBuffer = null;
+    int currentIndex = 0;
     boolean isLast = false;
     boolean isClosed = false;
 
-
     @Override
     public int read() {
-        if ( buffer.isEmpty() && isLast ) {
-            return -1;
+        if (currentBuffer == null || currentIndex >= currentBuffer.length) {
+            if (bufferQueue.isEmpty() && isLast) {
+                return -1;
+            }
+            try {
+                currentBuffer = bufferQueue.take();
+                currentIndex = 0;
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
         }
-        try {
-            return buffer.take() & 0xFF;
-        } catch ( InterruptedException e ) {
-            throw new RuntimeException( e );
-        }
+        return currentBuffer[currentIndex++] & 0xFF;
     }
 
-
-    public StreamAcknowledgement appendFrame( StreamFrame frame ) {
+    public StreamAcknowledgement appendFrame(StreamFrame frame) {
         byte[] data = frame.getBinary().toByteArray();
         try {
-            for ( byte element : data ) {
-                buffer.put( element );
-            }
-        } catch ( InterruptedException e ) {
-            throw new RuntimeException( e );
+            bufferQueue.put(data);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
         isLast = frame.getIsLast();
-        return StreamAcknowledgement.newBuilder().setCloseStream( isClosed ).build();
+        return StreamAcknowledgement.newBuilder().setCloseStream(isClosed).build();
     }
-
 
     @Override
     public boolean isClosed() {
-        return false;
+        return isClosed;
     }
-
 
     @Override
     public void close() {
