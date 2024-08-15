f25a727bd8a1428dc4bade9341a111c45dbc774b
Rafael Biehler <mail@rafaelbiehler.com>
2024-08-15 13:51:51 +0200

    Revert "Changed defaultStore back to 'hsqldb', until I figure out how to not run the query on MongoDB"
    This reverts commit ac7d057fd5497c0e220cca4fc53d06f51f645bd0.

--- dbms/src/main/java/org/polypheny/db/PolyphenyDb.java
+++ dbms/src/main/java/org/polypheny/db/PolyphenyDb.java
@@ -130,7 +130,7 @@ public class PolyphenyDb {
     public boolean daemonMode = false;
 
     @Option(name = { "-defaultStore" }, description = "Type of default storeId")
-    public String defaultStoreName = "hsqldb";
+    public String defaultStoreName = "mongodb";
 
     @Option(name = { "-defaultSource" }, description = "Type of default source")
     public static String defaultSourceName = "csv";
