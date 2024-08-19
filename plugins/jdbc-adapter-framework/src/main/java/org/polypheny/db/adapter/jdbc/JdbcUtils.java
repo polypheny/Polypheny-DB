06502e62b375e2af360e256ca0ce91ad29fce0bb
datomo <dave.lengw@gmail.com>
2024-08-19 16:00:41 +0200

    removed code-smelly fallback

--- plugins/jdbc-adapter-framework/src/main/java/org/polypheny/db/adapter/jdbc/JdbcUtils.java
+++ plugins/jdbc-adapter-framework/src/main/java/org/polypheny/db/adapter/jdbc/JdbcUtils.java
@@ -174,9 +174,6 @@ public final class JdbcUtils {
                             return PolyFloat.ofNullable( (Number) o );
                         case Types.DECIMAL:
                             return PolyBigDecimal.ofNullable( (BigDecimal) o );
-                        case Types.JAVA_OBJECT:
-                            log.warn( "Unknown Java Object Type: " + o );
-                            // fallback
                     }
                 default:
                     throw new GenericRuntimeException( "not implemented " + reps[i] + " " + types[i] );
