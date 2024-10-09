7b3cffae8d6dca0ecc083b6c40427cea92cd86c5
Martin Vahlensieck <martin.vahlensieck@unibas.ch>
2024-10-09 18:32:36 +0200

    Fix fallout from rebase

--- plugins/prism-interface/src/main/java/org/polypheny/db/prisminterface/metaRetrieval/DbMetaRetriever.java
+++ plugins/prism-interface/src/main/java/org/polypheny/db/prisminterface/metaRetrieval/DbMetaRetriever.java
@@ -76,12 +76,12 @@ public class DbMetaRetriever {
 
     private static List<LogicalNamespace> getLogicalNamespaces( String namespacePattern, String namespaceType ) {
         Pattern catalogNamespacePattern = getPatternOrNull( namespacePattern );
-        List<LogicalNamespace> logicalNamespaces = Catalog.getInstance().getSnapshot().getNamespaces( catalogNamespacePattern );
+        List<LogicalNamespace> logicalNamespaces = Catalog.snapshot().getNamespaces( catalogNamespacePattern );
         if ( namespaceType == null ) {
             return logicalNamespaces;
         }
         DataModel catalogNamespaceType = DataModel.valueOf( namespaceType );
-        return logicalNamespaces.stream().filter( n -> n.getDataModel() == catalogNamespaceType ).collect( Collectors.toList() );
+        return logicalNamespaces.stream().filter( n -> n.getDataModel() == catalogNamespaceType ).toList();
     }
 
 
@@ -99,7 +99,7 @@ public class DbMetaRetriever {
 
 
     public static Namespace getNamespace( String namespaceName ) {
-        return getNamespaceMeta( Catalog.getInstance().getSnapshot().getNamespace( namespaceName ).orElseThrow() );
+        return getNamespaceMeta( Catalog.snapshot().getNamespace( namespaceName ).orElseThrow() );
     }
 
 
@@ -107,26 +107,19 @@ public class DbMetaRetriever {
     public static EntitiesResponse searchEntities( String namespaceName, String entityPattern ) {
         EntitiesResponse.Builder responseBuilder = EntitiesResponse.newBuilder();
         LogicalNamespace namespace = Catalog.getInstance().getSnapshot().getNamespace( namespaceName ).orElseThrow();
-        switch ( namespace.getDataModel() ) {
-            case RELATIONAL:
-                responseBuilder.addAllEntities( getRelationalEntities( namespace.getId(), entityPattern ) );
-                break;
-            case GRAPH:
-                responseBuilder.addAllEntities( getGraphEntities( namespace.getId(), entityPattern ) );
-                break;
-            case DOCUMENT:
-                responseBuilder.addAllEntities( getDocumentEntities( namespace.getId(), entityPattern ) );
-                break;
-        }
-        return responseBuilder.build();
+        return switch ( namespace.getDataModel() ) {
+            case RELATIONAL -> responseBuilder.addAllEntities( getRelationalEntities( namespace.getId(), entityPattern ) ).build();
+            case GRAPH -> responseBuilder.addAllEntities( getGraphEntities( namespace.getId(), entityPattern ) ).build();
+            case DOCUMENT -> responseBuilder.addAllEntities( getDocumentEntities( namespace.getId(), entityPattern ) ).build();
+        };
     }
 
 
     // Relational entities
     private static List<Entity> getRelationalEntities( long namespaceId, String entityPattern ) {
         Pattern catalogEntityPattern = getPatternOrNull( entityPattern );
-        final List<LogicalTable> tables = Catalog.getInstance().getSnapshot().rel().getTables( namespaceId, catalogEntityPattern );
-        return tables.stream().map( logicalTable -> buildEntityFromTable( getTableMeta( logicalTable ) ) ).collect( Collectors.toList() );
+        final List<LogicalTable> tables = Catalog.snapshot().rel().getTables( namespaceId, catalogEntityPattern );
+        return tables.stream().map( logicalTable -> buildEntityFromTable( getTableMeta( logicalTable ) ) ).toList();
     }
 
 
@@ -154,7 +147,7 @@ public class DbMetaRetriever {
 
 
     private static List<Column> getColumns( LogicalTable logicalTable ) {
-        return logicalTable.getColumns().stream().map( DbMetaRetriever::getColumnMeta ).collect( Collectors.toList() );
+        return logicalTable.getColumns().stream().map( DbMetaRetriever::getColumnMeta ).toList();
     }
 
 
@@ -162,12 +155,12 @@ public class DbMetaRetriever {
         if ( logicalTable.primaryKey == null ) {
             return false;
         }
-        return Catalog.getInstance().getSnapshot().rel().getPrimaryKey( logicalTable.primaryKey ).isPresent();
+        return Catalog.snapshot().rel().getPrimaryKey( logicalTable.primaryKey ).isPresent();
     }
 
 
     private static PrimaryKey getPrimaryKeyMeta( LogicalTable logicalTable ) {
-        LogicalPrimaryKey logicalPrimaryKey = Catalog.getInstance().getSnapshot().rel().getPrimaryKey( logicalTable.primaryKey ).orElseThrow();
+        LogicalPrimaryKey logicalPrimaryKey = Catalog.snapshot().rel().getPrimaryKey( logicalTable.primaryKey ).orElseThrow();
         return PrimaryKey.newBuilder()
                 .setDatabaseName( logicalPrimaryKey.getSchemaName() )
                 .setNamespaceName( logicalPrimaryKey.getSchemaName() )
@@ -201,8 +194,8 @@ public class DbMetaRetriever {
 
 
     private static List<ForeignKey> getForeignKeys( LogicalTable logicalTable ) {
-        return Catalog.getInstance().getSnapshot().rel().getForeignKeys( logicalTable.getId() )
-                .stream().map( DbMetaRetriever::getForeignKeyMeta ).collect( Collectors.toList() );
+        return Catalog.snapshot().rel().getForeignKeys( logicalTable.getId() )
+                .stream().map( DbMetaRetriever::getForeignKeyMeta ).toList();
     }
 
 
@@ -220,19 +213,19 @@ public class DbMetaRetriever {
 
 
     private static List<Column> getReferencedColumns( LogicalForeignKey logicalForeignKey ) {
-        return logicalForeignKey.referencedKeyFieldIds.stream().map( id -> Catalog.snapshot().rel().getColumn( id ).orElseThrow() ).map( DbMetaRetriever::getColumnMeta ).collect( Collectors.toList() );
+        return logicalForeignKey.referencedKeyFieldIds.stream().map( id -> Catalog.snapshot().rel().getColumn( id ).orElseThrow() ).map( DbMetaRetriever::getColumnMeta ).toList();
     }
 
 
     private static List<ForeignKey> getExportedKeys( LogicalTable logicalTable ) {
         return Catalog.getInstance().getSnapshot().rel().getExportedKeys( logicalTable.getId() )
-                .stream().map( DbMetaRetriever::getForeignKeyMeta ).collect( Collectors.toList() );
+                .stream().map( DbMetaRetriever::getForeignKeyMeta ).toList();
     }
 
 
     private static List<Index> getIndexes( LogicalTable logicalTable, boolean unique ) {
         return Catalog.getInstance().getSnapshot().rel().getIndexes( logicalTable.getId(), unique )
-                .stream().map( DbMetaRetriever::getIndexMeta ).collect( Collectors.toList() );
+                .stream().map( DbMetaRetriever::getIndexMeta ).toList();
     }
 
 
@@ -311,7 +304,7 @@ public class DbMetaRetriever {
 
     static ClientInfoPropertyMetaResponse getClientInfoProperties() {
         List<ClientInfoPropertyMeta> propertyInfoMetas = PIClientInfoProperties.DEFAULTS.stream()
-                .map( DbMetaRetriever::getClientInfoPropertyMeta ).collect( Collectors.toList() );
+                .map( DbMetaRetriever::getClientInfoPropertyMeta ).toList();
         return ClientInfoPropertyMetaResponse.newBuilder()
                 .addAllClientInfoPropertyMetas( propertyInfoMetas )
                 .build();
@@ -334,7 +327,7 @@ public class DbMetaRetriever {
                 .map( org.polypheny.db.nodes.Function.class::cast )
                 .filter( f -> f.getFunctionCategory() == functionCategory || functionCategory == null )
                 .map( DbMetaRetriever::getFunctionMeta )
-                .collect( Collectors.toList() );
+                .toList();
         return FunctionsResponse.newBuilder().addAllFunctions( functions ).build();
     }
 
