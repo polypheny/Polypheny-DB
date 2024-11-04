63b6bb8263b06d4ea1e0eee822fee9ed70351ae5
Tobias Hafner <mail@tobiashafner.ch>
2024-11-04 14:39:04 +0100

    Implement retrieval of lock types from alg nodes

--- core/src/main/java/org/polypheny/db/algebra/core/common/Modify.java
+++ core/src/main/java/org/polypheny/db/algebra/core/common/Modify.java
@@ -75,4 +75,9 @@ public abstract class Modify<E extends Entity> extends SingleAlg {
     public enum Operation {
         INSERT, UPDATE, DELETE, MERGE
     }
+
+    public boolean isDataModifying() {
+        return true;
+    }
+
 }
