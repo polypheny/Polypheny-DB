9a990c9909bea2dd8718b05c78a3793c6e1d2ccc
Tobias Hafner <mail@tobiashafner.ch>
2024-12-13 10:54:42 +0100

    Not much progress on identifiers so far

--- core/src/main/java/org/polypheny/db/algebra/AlgStructuredTypeFlattener.java
+++ core/src/main/java/org/polypheny/db/algebra/AlgStructuredTypeFlattener.java
@@ -582,8 +582,8 @@ public class AlgStructuredTypeFlattener implements AlgConsumingVisitor {
         LogicalRelIdentifierInjection newAlg =
                 LogicalRelIdentifierInjection.create(
                         alg.getEntity(),
-                        getNewForOldRel( alg.getInput() ),
-                        alg.getRowType());
+                        getNewForOldRel( alg.getLeft() ),
+                        getNewForOldRel( alg.getRight() ));
         setNewForOldAlg( alg, newAlg );
     }
 
--- core/src/main/java/org/polypheny/db/algebra/enumerable/EnumerableIdentifierInjection.java
+++ core/src/main/java/org/polypheny/db/algebra/enumerable/EnumerableIdentifierInjection.java
@@ -16,45 +16,47 @@
 
 package org.polypheny.db.algebra.enumerable;
 
+import java.lang.reflect.Modifier;
+import java.util.List;
 import lombok.Getter;
+import org.apache.calcite.linq4j.Enumerable;
+import org.apache.calcite.linq4j.function.Function;
+import org.apache.calcite.linq4j.function.Function0;
 import org.apache.calcite.linq4j.tree.BlockBuilder;
 import org.apache.calcite.linq4j.tree.Expression;
 import org.apache.calcite.linq4j.tree.Expressions;
-import org.polypheny.db.algebra.AlgCollationTraitDef;
-import org.polypheny.db.algebra.AlgDistributionTraitDef;
+import org.apache.calcite.linq4j.tree.FunctionExpression;
+import org.apache.calcite.linq4j.tree.MethodCallExpression;
+import org.apache.calcite.linq4j.tree.ParameterExpression;
+import org.apache.calcite.linq4j.tree.Types;
+import org.polypheny.db.adapter.DataContext;
 import org.polypheny.db.algebra.AlgNode;
-import org.polypheny.db.algebra.SingleAlg;
-import org.polypheny.db.algebra.metadata.AlgMdCollation;
-import org.polypheny.db.algebra.metadata.AlgMdDistribution;
+import org.polypheny.db.algebra.core.common.Streamer;
 import org.polypheny.db.algebra.metadata.AlgMetadataQuery;
 import org.polypheny.db.catalog.entity.Entity;
 import org.polypheny.db.plan.AlgCluster;
 import org.polypheny.db.plan.AlgOptCost;
 import org.polypheny.db.plan.AlgPlanner;
 import org.polypheny.db.plan.AlgTraitSet;
+import org.polypheny.db.util.BuiltInMethod;
 
 @Getter
-public class EnumerableIdentifierInjection extends SingleAlg implements EnumerableAlg{
+public class EnumerableIdentifierInjection extends Streamer implements EnumerableAlg {
 
     public final Entity entity;
 
-    public EnumerableIdentifierInjection( Entity entity, AlgCluster cluster, AlgTraitSet traitSet, AlgNode input) {
-        super(cluster, traitSet, input);
+
+    public EnumerableIdentifierInjection( Entity entity, AlgCluster cluster, AlgTraitSet traitSet, AlgNode provider, AlgNode collector ) {
+        super( cluster, traitSet, provider, collector );
         this.entity = entity;
-        this.rowType = input.getTupleType();
-        assert getConvention() instanceof EnumerableConvention;
-        assert getConvention() == input.getConvention();
     }
 
-    public static EnumerableIdentifierInjection create(Entity table, AlgNode input) {
-        final AlgCluster cluster = input.getCluster();
-        final AlgMetadataQuery mq = cluster.getMetadataQuery();
-        final AlgTraitSet traitSet =
-                cluster.traitSetOf( EnumerableConvention.INSTANCE )
-                        .replaceIfs( AlgCollationTraitDef.INSTANCE, () -> AlgMdCollation.limit( mq, input ) )
-                        .replaceIf( AlgDistributionTraitDef.INSTANCE, () -> AlgMdDistribution.limit( mq, input ) );
-        return new EnumerableIdentifierInjection( table, cluster, traitSet, input );
+
+    public static EnumerableIdentifierInjection create( AlgTraitSet traitSet, Entity table, AlgNode provider, AlgNode collector ) {
+        final AlgCluster cluster = provider.getCluster();
+        return new EnumerableIdentifierInjection( table, cluster, traitSet, provider, collector );
     }
+
 
     @Override
     public AlgOptCost computeSelfCost( AlgPlanner planner, AlgMetadataQuery mq ) {
@@ -64,27 +66,30 @@ public class EnumerableIdentifierInjection extends SingleAlg implements Enumerab
 
 
     @Override
-    public String algCompareString() {
-        return this.getClass().getSimpleName() + "$" +
-                input.algCompareString() + "$" +
-                entity.hashCode()+ "&";
+    public EnumerableIdentifierInjection copy( AlgTraitSet traitSet, List<AlgNode> newInputs ) {
+        AlgCluster cluster = newInputs.get(0).getCluster();
+        return new EnumerableIdentifierInjection( entity, cluster, traitSet, newInputs.get(0), newInputs.get(1) );
     }
 
 
     @Override
     public Result implement( EnumerableAlgImplementor implementor, Prefer pref ) {
+        //TODO TH: this is a copy of the enumerable streamer use for testing
         final BlockBuilder builder = new BlockBuilder();
-        final EnumerableAlg input = (EnumerableAlg) getInput();
-
-        final Result result = implementor.visitChild( this, 0, input, pref );
-        final PhysType physicalType = PhysTypeImpl.of( implementor.getTypeFactory(), getTupleType(), result.format() );
-
-        Expression expression = builder.append( "input", result.block() );
-
-        // ToDo: actually do something here...
-
-
-        builder.add( Expressions.return_( null, expression ) );
-        return implementor.result( physicalType, builder.toBlock() );
+        final Result query = implementor.visitChild( this, 0, (EnumerableAlg) getLeft(), pref );
+        final Result prepared = implementor.visitChild( this, 1, (EnumerableAlg) getRight(), pref );
+        Expression executor = builder.append( builder.newName( "executor" + System.nanoTime() ), prepared.block() );
+        ParameterExpression exp = Expressions.parameter( Types.of( Function0.class, Enumerable.class ), builder.newName( "executor" + System.nanoTime() ) );
+        FunctionExpression<Function<?>> expCall = Expressions.lambda( Expressions.block( Expressions.return_( null, executor ) ) );
+        builder.add( Expressions.declare( Modifier.FINAL, exp, expCall ) );
+        MethodCallExpression transformContext = Expressions.call(
+                BuiltInMethod.STREAM_RIGHT.method,
+                Expressions.constant( DataContext.ROOT ),
+                builder.append( builder.newName( "query" + System.nanoTime() ), query.block() ),
+                exp,
+                Expressions.constant( getLeft().getTupleType().getFields().stream().map( f -> f.getType().getPolyType() ).toList() ) );
+        builder.add( Expressions.return_( null, builder.append( "test", transformContext ) ) );
+        return implementor.result( prepared.physType(), builder.toBlock() );
     }
+
 }
