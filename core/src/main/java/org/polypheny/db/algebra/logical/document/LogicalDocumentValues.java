db27ed215ba7680bdfcf45394fcbfd29e019bf5c
Tobias Hafner <mail@tobiashafner.ch>
2024-12-24 23:46:59 +0100

    Check for identifier field in values

--- core/src/main/java/org/polypheny/db/algebra/logical/document/LogicalDocumentValues.java
+++ core/src/main/java/org/polypheny/db/algebra/logical/document/LogicalDocumentValues.java
@@ -30,6 +30,7 @@ import org.polypheny.db.plan.AlgCluster;
 import org.polypheny.db.plan.AlgTraitSet;
 import org.polypheny.db.plan.Convention;
 import org.polypheny.db.rex.RexDynamicParam;
+import org.polypheny.db.transaction.locking.IdentifierUtils;
 import org.polypheny.db.schema.trait.ModelTrait;
 import org.polypheny.db.type.entity.document.PolyDocument;
 
@@ -65,6 +66,7 @@ public class LogicalDocumentValues extends DocumentValues implements RelationalT
      */
     public LogicalDocumentValues( AlgCluster cluster, AlgTraitSet traitSet, List<PolyDocument> document ) {
         super( cluster, traitSet, document );
+        IdentifierUtils.throwIfContainsIdentifierKey( document );
     }
 
 
