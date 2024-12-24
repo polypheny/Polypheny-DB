b50cfcc7baa08bfe7318526a7df53e82c282c7e6
Tobias Hafner <mail@tobiashafner.ch>
2024-12-24 17:02:34 +0100

    Start debugging lpg insertions

--- core/src/main/java/org/polypheny/db/algebra/enumerable/lpg/EnumerableLpgIdentifier.java
+++ core/src/main/java/org/polypheny/db/algebra/enumerable/lpg/EnumerableLpgIdentifier.java
@@ -25,9 +25,7 @@ import org.polypheny.db.algebra.core.common.Identifier;
 import org.polypheny.db.algebra.enumerable.EnumerableAlg;
 import org.polypheny.db.algebra.enumerable.EnumerableAlgImplementor;
 import org.polypheny.db.algebra.enumerable.EnumerableConvention;
-import org.polypheny.db.algebra.enumerable.EnumerableRelIdentifier;
 import org.polypheny.db.algebra.enumerable.PhysType;
-import org.polypheny.db.algebra.enumerable.document.EnumerableDocIdentifier;
 import org.polypheny.db.algebra.metadata.AlgMetadataQuery;
 import org.polypheny.db.catalog.entity.Entity;
 import org.polypheny.db.plan.AlgCluster;
@@ -50,9 +48,10 @@ public class EnumerableLpgIdentifier extends Identifier implements EnumerableAlg
         return planner.getCostFactory().makeCost( dRows, 0, 0 );
     }
 
+
     @Override
     public AlgNode copy( AlgTraitSet traitSet, List<AlgNode> inputs ) {
-        return new EnumerableLpgIdentifier( inputs.get(0).getCluster(), traitSet, entity, inputs.get( 0 ) );
+        return new EnumerableLpgIdentifier( inputs.get( 0 ).getCluster(), traitSet, entity, inputs.get( 0 ) );
     }
 
 
@@ -64,8 +63,8 @@ public class EnumerableLpgIdentifier extends Identifier implements EnumerableAlg
         final PhysType physType = result.physType();
 
         Expression input_ = builder.append( "input", result.block() );
-        Expression entity_ = Expressions.constant(entity.getId());
-        Expression identification_ = builder.append( "identification", Expressions.call( BuiltInMethod.ADD_REL_IDENTIFIERS.method, input_, entity_ ) );
+        Expression entity_ = Expressions.constant( entity.getId() );
+        Expression identification_ = builder.append( "identification", Expressions.call( BuiltInMethod.ADD_LPG_IDENTIFIERS.method, input_, entity_ ) );
 
         builder.add( Expressions.return_( null, identification_ ) );
         return implementor.result( physType, builder.toBlock() );
