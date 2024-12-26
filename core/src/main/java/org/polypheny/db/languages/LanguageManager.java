5fc384cff6cb7242590357984819ff7271213477
Tobias Hafner <mail@tobiashafner.ch>
2024-12-26 11:33:42 +0100

    Add alg nodes for identifier collection

--- core/src/main/java/org/polypheny/db/languages/LanguageManager.java
+++ core/src/main/java/org/polypheny/db/languages/LanguageManager.java
@@ -43,6 +43,7 @@ import org.polypheny.db.transaction.Statement;
 import org.polypheny.db.transaction.Transaction;
 import org.polypheny.db.transaction.locking.IdentifierAdder;
 import org.polypheny.db.type.entity.PolyValue;
+import org.polypheny.db.transaction.TransactionManager;
 import org.polypheny.db.transaction.locking.AlgTreeRewriter;
 import org.polypheny.db.util.DeadlockException;
 import org.polypheny.db.util.Pair;
