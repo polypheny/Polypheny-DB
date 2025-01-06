fb5a99dd4c72d382fb074fa830e0685723d3b6e7
Tobias Hafner <mail@tobiashafner.ch>
2025-01-06 14:10:32 +0100

    Modify transaction to have a write set instead of read set

--- dbms/src/main/java/org/polypheny/db/transaction/TransactionImpl.java
+++ dbms/src/main/java/org/polypheny/db/transaction/TransactionImpl.java
@@ -141,7 +141,7 @@ public class TransactionImpl implements Transaction, Comparable<Object> {
 
     private Set<Entity> writtenEntities = new HashSet<>();
 
-    private Set<VersionedEntryIdentifier> readSet = new HashSet<>(); // This only contains entries if the transaction involves entities in MVCC mode
+    private Set<VersionedEntryIdentifier> writeSet = new HashSet<>(); // This only contains entries if the transaction involves entities in MVCC mode
 
 
     TransactionImpl(
