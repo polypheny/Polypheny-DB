1580479529308e5844aef771356d3e9d0671c583
Tobias Hafner <mail@tobiashafner.ch>
2025-01-07 20:46:29 +0100

    Add version identifier column

--- dbms/src/main/java/org/polypheny/db/PolyphenyDb.java
+++ dbms/src/main/java/org/polypheny/db/PolyphenyDb.java
@@ -125,9 +125,9 @@ public class PolyphenyDb {
     public boolean daemonMode = false;
 
     @Option(name = { "-defaultStore" }, description = "Type of default storeId")
-    //public String defaultStoreName = "hsqldb";
+    public String defaultStoreName = "hsqldb";
     //public String defaultStoreName = "neo4j";
-    public String defaultStoreName = "mongodb";
+    //public String defaultStoreName = "mongodb";
 
     @Option(name = { "-defaultSource" }, description = "Type of default source")
     public static String defaultSourceName = "csv";
