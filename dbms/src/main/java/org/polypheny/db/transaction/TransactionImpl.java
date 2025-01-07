fd8381ae9dcfcdae28da075e5ee3b0151a4e9eb4
Tobias Hafner <mail@tobiashafner.ch>
2025-01-07 21:03:32 +0100

    Add version information when rewriting

--- dbms/src/main/java/org/polypheny/db/transaction/TransactionImpl.java
+++ dbms/src/main/java/org/polypheny/db/transaction/TransactionImpl.java
@@ -465,6 +465,12 @@ public class TransactionImpl implements Transaction, Comparable<Object> {
 
 
     @Override
+    public long getTransactionTimestamp() {
+        return transactionTimestamp;
+    }
+
+
+    @Override
     public void wakeup() {
         executor.interrupt();
     }
