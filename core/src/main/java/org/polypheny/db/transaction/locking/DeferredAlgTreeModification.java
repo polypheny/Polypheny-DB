6d4c8faeb00277143187eb9e15390edb2ce616ac
Tobias Hafner <mail@tobiashafner.ch>
2025-01-16 15:11:17 +0100

    rewrite scans to ignore soft deleted entries

--- core/src/main/java/org/polypheny/db/transaction/locking/DeferredAlgTreeModification.java
+++ core/src/main/java/org/polypheny/db/transaction/locking/DeferredAlgTreeModification.java
@@ -1,19 +1,42 @@
 package org.polypheny.db.transaction.locking;
 
+import com.google.common.collect.ImmutableSet;
 import java.util.List;
+import java.util.Set;
 import org.apache.commons.lang3.NotImplementedException;
+import org.polypheny.db.algebra.AlgCollations;
 import org.polypheny.db.algebra.AlgNode;
+import org.polypheny.db.algebra.core.AggregateCall;
+import org.polypheny.db.algebra.core.JoinAlgType;
+import org.polypheny.db.algebra.logical.relational.LogicalRelAggregate;
+import org.polypheny.db.algebra.logical.relational.LogicalRelFilter;
+import org.polypheny.db.algebra.logical.relational.LogicalRelJoin;
+import org.polypheny.db.algebra.logical.relational.LogicalRelProject;
+import org.polypheny.db.algebra.operators.OperatorName;
 import org.polypheny.db.algebra.type.AlgDataType;
+import org.polypheny.db.algebra.type.AlgDataTypeFactoryImpl;
 import org.polypheny.db.config.RuntimeConfig;
+import org.polypheny.db.languages.OperatorRegistry;
 import org.polypheny.db.languages.QueryLanguage;
 import org.polypheny.db.nodes.Node;
 import org.polypheny.db.processing.Processor;
 import org.polypheny.db.processing.QueryContext;
 import org.polypheny.db.processing.QueryContext.ParsedQueryContext;
+import org.polypheny.db.rex.RexCall;
+import org.polypheny.db.rex.RexIndexRef;
+import org.polypheny.db.rex.RexLiteral;
 import org.polypheny.db.transaction.Statement;
+import org.polypheny.db.type.PolyType;
+import org.polypheny.db.type.PolyTypeFactoryImpl;
+import org.polypheny.db.type.entity.numerical.PolyLong;
+import org.polypheny.db.util.ImmutableBitSet;
 import org.polypheny.db.util.Pair;
 
 public class DeferredAlgTreeModification {
+
+    private static final AlgDataType BOOLEAN_ALG_TYPE = ((PolyTypeFactoryImpl) AlgDataTypeFactoryImpl.DEFAULT).createBasicPolyType( PolyType.BOOLEAN, true );
+    private static final AlgDataType INTEGER_ALG_TYPE = ((PolyTypeFactoryImpl) AlgDataTypeFactoryImpl.DEFAULT).createBasicPolyType( PolyType.INTEGER, true );
+
 
     private final AlgNode target;
     private final Modification modification;
@@ -63,27 +86,149 @@ public class DeferredAlgTreeModification {
 
 
     private AlgNode applyLimitRelScanToSnapshot() {
-        String queryTemplate = """
-                    SELECT t1.*
-                    FROM %s t1
-                    JOIN (
-                        SELECT _eid, MAX(_vid) AS max_vid
-                        FROM %s
-                        WHERE (_vid > 0 AND _vid <= %d)
-                        OR _vid = %d
-                        GROUP BY _eid
-                        ) t2
-                        ON t1._eid = t2._eid AND t1._vid = t2.max_vid;
-                """;
+        LogicalRelFilter scopeFilter = buildRelScopeFilter( target );
+        LogicalRelProject identifierVersionProject = createRelIdentifierVersionProject( scopeFilter );
+        LogicalRelAggregate rightAggregate = createRelVersionAggregate( identifierVersionProject );
+        return createVersionScanJoin( target, rightAggregate );
+    }
 
-        String query = String.format(
-                queryTemplate,
-                target.getEntity().getName(),
-                target.getEntity().getName(),
-                statement.getTransaction().getSequenceNumber(),
-                statement.getTransaction().getSequenceNumber());
 
-        return generateSubtree( query, QueryLanguage.from( "sql" ) ).getInput( 0 );
+    private LogicalRelProject createRelIdentifierVersionProject( LogicalRelFilter input ) {
+        return LogicalRelProject.create(
+                input,
+                List.of(
+                        new RexIndexRef( 0, IdentifierUtils.IDENTIFIER_ALG_TYPE ),
+                        new RexIndexRef( 1, IdentifierUtils.VERSION_ALG_TYPE )
+                ),
+                List.of(
+                        IdentifierUtils.IDENTIFIER_KEY,
+                        IdentifierUtils.VERSION_KEY
+                )
+        );
+    }
+
+
+    private LogicalRelAggregate createRelVersionAggregate( LogicalRelProject input ) {
+        ImmutableBitSet rightAggregateGroupSet = ImmutableBitSet.of( 0 );
+        AggregateCall rightAggregateCall = AggregateCall.create(
+                OperatorRegistry.getAgg( OperatorName.MAX ),
+                false,
+                false,
+                List.of( 1 ),
+                -1,
+                AlgCollations.EMPTY,
+                IdentifierUtils.VERSION_ALG_TYPE,
+                "max_vid"
+        );
+
+        return LogicalRelAggregate.create(
+                input,
+                rightAggregateGroupSet,
+                List.of( rightAggregateGroupSet ),
+                List.of( rightAggregateCall )
+        );
+    }
+
+
+    private LogicalRelJoin createVersionScanJoin( AlgNode left, LogicalRelAggregate right ) {
+        RexCall identifierJoinCondition = new RexCall(
+                BOOLEAN_ALG_TYPE,
+                OperatorRegistry.get( OperatorName.EQUALS ),
+                new RexIndexRef( 0, IdentifierUtils.IDENTIFIER_ALG_TYPE ),
+                new RexIndexRef( 4, IdentifierUtils.IDENTIFIER_ALG_TYPE )
+        );
+
+        RexCall versionJoinCondition = new RexCall(
+                BOOLEAN_ALG_TYPE,
+                OperatorRegistry.get( OperatorName.EQUALS ),
+                new RexIndexRef( 1, IdentifierUtils.VERSION_ALG_TYPE ),
+                new RexIndexRef( 5, IdentifierUtils.VERSION_ALG_TYPE )
+        );
+
+        RexCall joinCondition = new RexCall(
+                BOOLEAN_ALG_TYPE,
+                OperatorRegistry.get( OperatorName.AND ),
+                identifierJoinCondition,
+                versionJoinCondition
+        );
+
+        return LogicalRelJoin.create(
+                left,
+                right,
+                joinCondition,
+                Set.of(),
+                JoinAlgType.INNER
+        );
+    }
+
+
+    private LogicalRelFilter buildRelScopeFilter( AlgNode input ) {
+        RexCall selfReadCondition = new RexCall(
+                BOOLEAN_ALG_TYPE,
+                OperatorRegistry.get( OperatorName.EQUALS ),
+                new RexIndexRef( 1, IdentifierUtils.VERSION_ALG_TYPE ),
+                new RexLiteral(
+                        IdentifierUtils.getVersionAsPolyLong( statement.getTransaction().getSequenceNumber(), false ),
+                        IdentifierUtils.VERSION_ALG_TYPE,
+                        PolyType.BIGINT
+                )
+        );
+
+        RexCall versionInSnapshotCondition = new RexCall(
+                BOOLEAN_ALG_TYPE,
+                OperatorRegistry.get( OperatorName.LESS_THAN_OR_EQUAL ),
+                new RexIndexRef( 1, IdentifierUtils.VERSION_ALG_TYPE ),
+                new RexLiteral(
+                        IdentifierUtils.getVersionAsPolyLong( statement.getTransaction().getSequenceNumber(), true ),
+                        IdentifierUtils.VERSION_ALG_TYPE,
+                        PolyType.BIGINT
+                )
+        );
+
+        RexCall versionCommittedCondition = new RexCall(
+                BOOLEAN_ALG_TYPE,
+                OperatorRegistry.get( OperatorName.GREATER_THAN_OR_EQUAL ),
+                new RexIndexRef( 1, IdentifierUtils.VERSION_ALG_TYPE ),
+                new RexLiteral(
+                        PolyLong.of( 0 ),
+                        IdentifierUtils.VERSION_ALG_TYPE,
+                        PolyType.BIGINT
+                )
+        );
+
+        RexCall notDeletedCondition = new RexCall(
+                BOOLEAN_ALG_TYPE,
+                OperatorRegistry.get( OperatorName.GREATER_THAN_OR_EQUAL ),
+                new RexIndexRef( 0, IdentifierUtils.IDENTIFIER_ALG_TYPE ),
+                new RexLiteral(
+                        PolyLong.of( 0 ),
+                        IdentifierUtils.VERSION_ALG_TYPE,
+                        PolyType.BIGINT
+                )
+        );
+
+        RexCall scopeCondition = new RexCall(
+                BOOLEAN_ALG_TYPE,
+                OperatorRegistry.get( OperatorName.AND ),
+                notDeletedCondition,
+                new RexCall(
+                        BOOLEAN_ALG_TYPE,
+                        OperatorRegistry.get( OperatorName.OR ),
+                        selfReadCondition,
+                        new RexCall(
+                                BOOLEAN_ALG_TYPE,
+                                OperatorRegistry.get( OperatorName.AND ),
+                                versionInSnapshotCondition,
+                                versionCommittedCondition
+                        )
+                )
+        );
+
+        return LogicalRelFilter.create(
+                input,
+                scopeCondition,
+                ImmutableSet.of()
+        );
     }
 
 
@@ -95,32 +240,4 @@ public class DeferredAlgTreeModification {
     private AlgNode applyLimitLpgScanToSnapshot( AlgNode parent ) {
         throw new NotImplementedException();
     }
-
-
-    private AlgNode generateSubtree( String query, QueryLanguage language ) {
-        QueryContext context = QueryContext.builder()
-                .query( query )
-                .language( language )
-                .origin( statement.getTransaction().getOrigin() )
-                .namespaceId( target.getEntity().getNamespaceId() )
-                .transactionManager( statement.getTransaction().getTransactionManager() )
-                .isMvccInternal( true )
-                .build();
-
-        Processor processor = context.getLanguage().processorSupplier().get();
-        List<QueryContext.ParsedQueryContext> parsedQueries = context.getLanguage().parser().apply( context );
-        assert parsedQueries.size() == 1;
-        QueryContext.ParsedQueryContext parsed = parsedQueries.get(0);
-
-        if (parsed.getLanguage().validatorSupplier() != null) {
-            Pair<Node, AlgDataType> validated = processor.validate(
-                    statement.getTransaction(),
-                    parsed.getQueryNode().get(),
-                    RuntimeConfig.ADD_DEFAULT_VALUES_IN_INSERTS.getBoolean() );
-            parsed = ParsedQueryContext.fromQuery( parsed.getQuery(), validated.left, parsed );
-        }
-
-        return processor.translate( statement, parsed ).alg;
-    }
-
 }
