4bd2367181d50aa0cb11b23a322393e9010b31b5
Tobias Hafner <mail@tobiashafner.ch>
2025-01-20 12:36:33 +0100

    Update management of lockables

--- plugins/sql-language/src/main/java/org/polypheny/db/sql/language/ddl/SqlAlterInterfacesAdd.java
+++ plugins/sql-language/src/main/java/org/polypheny/db/sql/language/ddl/SqlAlterInterfacesAdd.java
@@ -118,7 +118,7 @@ public class SqlAlterInterfacesAdd extends SqlAlter {
 
     @Override
     public Map<Lockable, LockType> deriveLockables( Context context, ParsedQueryContext parsedQueryContext ) {
-        return LockableUtils.getMapWithGlobalLockable(LockType.EXCLUSIVE);
+        return LockableUtils.getMapOfGlobalLockable(LockType.EXCLUSIVE);
     }
 }
 
