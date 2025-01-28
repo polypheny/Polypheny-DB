ec444d8ff6778fd31c2d55321ee33cffda81a792
Tobias Hafner <mail@tobiashafner.ch>
2025-01-28 21:51:42 +0100

    Start implementing workaround for graph validation

--- core/src/main/java/org/polypheny/db/transaction/locking/MvccUtils.java
+++ core/src/main/java/org/polypheny/db/transaction/locking/MvccUtils.java
@@ -16,6 +16,7 @@
 
 package org.polypheny.db.transaction.locking;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
@@ -32,7 +33,6 @@ import org.polypheny.db.processing.QueryContext;
 import org.polypheny.db.transaction.Transaction;
 import org.polypheny.db.type.entity.PolyString;
 import org.polypheny.db.type.entity.PolyValue;
-import org.polypheny.db.util.PrecedenceClimbingParser.Result;
 
 public class MvccUtils {
 
@@ -60,7 +60,8 @@ public class MvccUtils {
         return maxVersion <= sequenceNumber;
     }
 
-    private static ExecutedContext executeStatement(Entity entitiy, Transaction transaction, QueryLanguage language, String query) {
+
+    private static ExecutedContext executeStatement( Entity entitiy, Transaction transaction, QueryLanguage language, String query ) {
         ImplementationContext context = LanguageManager.getINSTANCE().anyPrepareQuery(
                 QueryContext.builder()
                         .query( query )
@@ -97,19 +98,20 @@ public class MvccUtils {
         }
     }
 
-    private static long validateDocWrites(long sequenceNumber, Entity writtenEntity, Transaction transaction ) {
+
+    private static long validateDocWrites( long sequenceNumber, Entity writtenEntity, Transaction transaction ) {
         // ToDo TH. Replace with more efficient mechanism once group and addToSet are fixed
 
         // step 1: get list of all written document entry ids
         String writeSetTemplate = "db.%s.find( { \"_vid\": %d }, { \"_eid\": 1 } );";
 
-        String writeSetQuery = String.format( writeSetTemplate, writtenEntity.getName(), -sequenceNumber);
+        String writeSetQuery = String.format( writeSetTemplate, writtenEntity.getName(), -sequenceNumber );
         List<List<PolyValue>> res;
         HashSet<Long> entryIds = new HashSet<>();
         try ( ResultIterator iterator = executeStatement( writtenEntity, transaction, QueryLanguage.from( "mql" ), writeSetQuery ).getIterator() ) {
-            while (iterator.hasMoreRows()) {
+            while ( iterator.hasMoreRows() ) {
                 res = iterator.getNextBatch();
-                res.forEach( r -> entryIds.add(r.get(0).asDocument().get( IdentifierUtils.getIdentifierKeyAsPolyString() ).asLong().longValue()  ) );
+                res.forEach( r -> entryIds.add( r.get( 0 ).asDocument().get( IdentifierUtils.getIdentifierKeyAsPolyString() ).asLong().longValue() ) );
             }
         }
 
@@ -119,41 +121,73 @@ public class MvccUtils {
                 + "    { \"$group\": { \"_id\": null, \"max_vid\": { \"$max\": \"$_vid\" } } }"
                 + "]);";
 
-        String entryIdString = entryIds.stream().map( String::valueOf ).collect( Collectors.joining(", "));
-        String getMaxQuery = String.format( getMaxTemplate, writtenEntity.getName(), entryIdString  );
+        String entryIdString = entryIds.stream().map( String::valueOf ).collect( Collectors.joining( ", " ) );
+        String getMaxQuery = String.format( getMaxTemplate, writtenEntity.getName(), entryIdString );
         try ( ResultIterator iterator = executeStatement( writtenEntity, transaction, QueryLanguage.from( "mql" ), getMaxQuery ).getIterator() ) {
             return Objects.requireNonNull( iterator.getNextBatch().get( 0 ).get( 0 ).asDocument().get( PolyString.of( "max_vid" ) ).asBigDecimal().getValue() ).longValue();
         }
     }
 
 
-    public static long validateGraphWrites(long sequenceNumber, Entity writtenEntity, Transaction transaction) {
-        String queryTemplate = """
-                MATCH (n)
-                WHERE n._eid IN (
-                    MATCH (m)
-                    WHERE m._vid = %d
-                    RETURN m._eid
-                )
-                RETURN MAX(n._vid) AS max_vid
-                UNION
-                MATCH ()-[r]->()
-                WHERE r._eid IN (
-                    MATCH (m)
-                    WHERE m._vid = %d
-                    RETURN m._eid
-                )
-                RETURN MAX(r._vid) AS max_vid
-                """;
-
-        String query = String.format( queryTemplate, sequenceNumber, -sequenceNumber);
+    public static long validateGraphWrites( long sequenceNumber, Entity writtenEntity, Transaction transaction ) {
+        // Step 1: get written node entry ids
+        String writeSetQuery = String.format(
+                "MATCH (m) WHERE m._vid = %d RETURN m._eid AS eids",
+                -sequenceNumber
+        );
         List<List<PolyValue>> res;
-        try ( ResultIterator iterator = executeStatement( writtenEntity, transaction, QueryLanguage.from( "cypher" ), query ).getIterator() ) {
-            res = iterator.getNextBatch();
-            return res.get( 0 ).get( 0 ).asLong().longValue();
+        List<Long> nodeEntryIds = new ArrayList<>();
+        try (ResultIterator iterator = executeStatement(writtenEntity, transaction, QueryLanguage.from("cypher"), writeSetQuery).getIterator()) {
+            while ( iterator.hasMoreRows() ) {
+                res = iterator.getNextBatch();
+                res.forEach( r -> nodeEntryIds.add( Long.parseLong(r.get(0).asString().getValue() ) ));
+            }
         }
+
+        // Step 2: find the maximum _vid for nodes
+        long maxVidNodes = -1;
+        for (long eid : nodeEntryIds) {
+            String getMaxNodesQuery = String.format(
+                    "MATCH (n) WHERE n._eid = %d RETURN MAX(n._vid) AS max_vid_nodes",
+                    eid
+            );
+            maxVidNodes = getMaxVidOfGraphElements( writtenEntity, transaction, maxVidNodes, getMaxNodesQuery );
+        }
+
+        // Step 3: get written relationship entry ids
+        String relWriteSetQuery = String.format(
+                "MATCH ()-[r]->() WHERE r._vid = %d RETURN r._eid AS eids",
+                -sequenceNumber
+        );
+        List<Long> relEntryIds = new ArrayList<>();
+        try (ResultIterator iterator = executeStatement(writtenEntity, transaction, QueryLanguage.from("cypher"), relWriteSetQuery).getIterator()) {
+            while (iterator.hasMoreRows()) {
+                res = iterator.getNextBatch();
+                res.forEach(r -> relEntryIds.add(Long.parseLong(r.get(0).asString().getValue())));
+            }
+        }
+
+        // Step 4: Find the maximum _vid for relationships
+        long maxVidRels = -1;
+        for (long eid : relEntryIds) {
+            String getMaxRelsQuery = String.format(
+                    "MATCH ()-[r]->() WHERE r._eid = %d RETURN MAX(r._vid) AS max_vid_rels",
+                    eid
+            );
+            maxVidRels = getMaxVidOfGraphElements( writtenEntity, transaction, maxVidRels, getMaxRelsQuery );
+        }
+
+        return Math.max(maxVidNodes, maxVidRels);
     }
 
+
+    private static long getMaxVidOfGraphElements( Entity writtenEntity, Transaction transaction, long maxVidNodes, String getMaxNodesQuery ) {
+        try ( ResultIterator iterator = executeStatement(writtenEntity, transaction, QueryLanguage.from("cypher"), getMaxNodesQuery).getIterator()) {
+            long currentMax = Objects.requireNonNull( iterator.getNextBatch().get( 0 ).get( 0 ).asBigDecimal().getValue() ).longValue();
+            maxVidNodes = Math.max( maxVidNodes, currentMax );
+        }
+        return maxVidNodes;
+    }
 
 
     public static long updateWrittenVersionIds( long sequenceNumber, Set<Entity> writtenEntities, Transaction transaction ) {
@@ -161,13 +195,14 @@ public class MvccUtils {
         for ( Entity writtenEntity : writtenEntities ) {
             switch ( writtenEntity.getDataModel() ) {
                 case RELATIONAL -> updateWrittenRelVersionIds( sequenceNumber, commitSequenceNumber, writtenEntity, transaction );
-                case DOCUMENT -> updateWrittenDocVersionIds(sequenceNumber, commitSequenceNumber, writtenEntity, transaction);
-                case GRAPH -> updateWrittenGraphVersionIds(sequenceNumber, commitSequenceNumber, writtenEntity, transaction);
+                case DOCUMENT -> updateWrittenDocVersionIds( sequenceNumber, commitSequenceNumber, writtenEntity, transaction );
+                case GRAPH -> updateWrittenGraphVersionIds( sequenceNumber, commitSequenceNumber, writtenEntity, transaction );
             }
         }
         SequenceNumberGenerator.getInstance().releaseNumber( sequenceNumber );
         return commitSequenceNumber;
     }
+
 
     private static void updateWrittenRelVersionIds( long sequenceNumber, long commitSequenceNumber, Entity writtenEntity, Transaction transaction ) {
         String queryTemplate = """
@@ -180,7 +215,10 @@ public class MvccUtils {
         executeStatement( writtenEntity, transaction, QueryLanguage.from( "sql" ), query );
     }
 
+
     private static void updateWrittenGraphVersionIds( long sequenceNumber, long commitSequenceNumber, Entity writtenEntity, Transaction transaction ) {
+        return;
+        /**
         String updateTemplate = """
                 MATCH (n)
                 WHERE n._vid = %d
@@ -193,30 +231,32 @@ public class MvccUtils {
                 SET r._vid = %d
                 """;
 
-        String updateQuery = String.format(updateTemplate,
+        String updateQuery = String.format( updateTemplate,
                 -sequenceNumber,
                 commitSequenceNumber,
                 -sequenceNumber,
-                commitSequenceNumber);
+                commitSequenceNumber );
 
         executeStatement( writtenEntity, transaction, QueryLanguage.from( "cypher" ), updateQuery );
+         **/
     }
 
 
     private static void updateWrittenDocVersionIds( long sequenceNumber, long commitSequenceNumber, Entity writtenEntity, Transaction transaction ) {
         String updateTemplate = """
-                db.%s.updateMany(
-                { _vid: %d },
-                { $set: { _vid: %d } }
-                );
-        """;
+                        db.%s.updateMany(
+                        { _vid: %d },
+                        { $set: { _vid: %d } }
+                        );
+                """;
 
-        String updateQuery = String.format(updateTemplate,
+        String updateQuery = String.format( updateTemplate,
                 writtenEntity.getName(),
                 -sequenceNumber,
-                commitSequenceNumber);
+                commitSequenceNumber );
         executeStatement( writtenEntity, transaction, QueryLanguage.from( "mql" ), updateQuery );
     }
+
 }
 
 
