44cefd74398eb188bc6803ad339ff0f522f8c92d
Tobias Hafner <mail@tobiashafner.ch>
2025-01-29 10:45:57 +0100

    Code cleanup and bugfix

--- core/src/main/java/org/polypheny/db/transaction/locking/DeferredAlgTreeModification.java
+++ core/src/main/java/org/polypheny/db/transaction/locking/DeferredAlgTreeModification.java
@@ -208,7 +208,6 @@ public class DeferredAlgTreeModification {
                         )
         );
 
-
         return LogicalRelFilter.create(
                 input,
                 scopeCondition,
