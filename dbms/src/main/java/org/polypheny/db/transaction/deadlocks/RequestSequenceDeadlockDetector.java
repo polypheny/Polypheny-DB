380f19436b6cdf7161e888c48b6ef1bc1abd6773
Tobias Hafner <mail@tobiashafner.ch>
2025-02-10 16:56:38 +0100

    Work in progress on doc updates

--- dbms/src/main/java/org/polypheny/db/transaction/deadlocks/RequestSequenceDeadlockDetector.java
+++ dbms/src/main/java/org/polypheny/db/transaction/deadlocks/RequestSequenceDeadlockDetector.java
@@ -1,67 +1,50 @@
-/*
- * Copyright 2019-2024 The Polypheny Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.polypheny.db.transaction.deadlocks;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
+import java.util.Map;
 import lombok.NonNull;
 import org.polypheny.db.transaction.Transaction;
 import org.polypheny.db.transaction.locking.Lockable;
 
 public class RequestSequenceDeadlockDetector implements DeadlockDetector {
 
-    Transaction criticalTransaction = null;
-    HashMap<Lockable, Long> lockableSequenceNumbers = new HashMap<>();
-    HashMap<Transaction, Long> transactionSequenceNumbers = new HashMap<>();
-
+    private Transaction criticalTransaction = null;
+    private final Map<Lockable, Long> lockableSequenceNumbers = new HashMap<>();
 
     @Override
-    public void add( @NonNull Lockable lockable, @NonNull Transaction newTransaction, @NonNull Set<Transaction> owners ) {
-        long transactionSequenceNumber = transactionSequenceNumbers.getOrDefault( newTransaction, System.nanoTime() );
-        if ( !lockableSequenceNumbers.containsKey( lockable ) ) {
-            lockableSequenceNumbers.put( lockable, transactionSequenceNumber );
-            transactionSequenceNumbers.put( newTransaction, transactionSequenceNumber );
+    public synchronized void add(@NonNull Lockable lockable, @NonNull Transaction newTransaction, @NonNull Set<Transaction> owners) {
+        long transactionSequenceNumber = newTransaction.getSequenceNumber();
+        if (!lockableSequenceNumbers.containsKey(lockable)) {
+            lockableSequenceNumbers.put(lockable, transactionSequenceNumber);
             return;
         }
-        long lockableSequenceNumber = lockableSequenceNumbers.get( lockable );
-        if ( transactionSequenceNumber > lockableSequenceNumber ) {
+
+        long existingSequenceNumber = lockableSequenceNumbers.get(lockable);
+
+        if (transactionSequenceNumber > existingSequenceNumber) {
             criticalTransaction = newTransaction;
             return;
         }
-        transactionSequenceNumbers.put( newTransaction, transactionSequenceNumber );
+
+        for (Transaction owner : owners) {
+            lockableSequenceNumbers.put(lockable, Math.min(lockableSequenceNumbers.get(lockable), owner.getSequenceNumber()));
+        }
     }
 
-
     @Override
-    public List<Transaction> getConflictingTransactions() {
-        if ( criticalTransaction == null ) {
+    public synchronized List<Transaction> getConflictingTransactions() {
+        if (criticalTransaction == null) {
             return List.of();
         }
-        List<Transaction> conflictingTransactions = List.of( criticalTransaction );
+        List<Transaction> conflictingTransactions = List.of(criticalTransaction);
         criticalTransaction = null;
         return conflictingTransactions;
     }
 
-
     @Override
-    public void remove( @NonNull Lockable lockable, @NonNull Transaction transaction ) {
-        lockableSequenceNumbers.remove( lockable );
-        transactionSequenceNumbers.remove( transaction );
+    public synchronized void remove(@NonNull Lockable lockable, @NonNull Transaction transaction) {
+        lockableSequenceNumbers.remove(lockable);
     }
-
 }
