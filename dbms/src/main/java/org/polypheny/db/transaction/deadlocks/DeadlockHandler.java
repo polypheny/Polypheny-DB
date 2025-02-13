2b5577e4967d3eddb3b47f5d4d6561e979c0f84a
Martin Vahlensieck <martin.vahlensieck@unibas.ch>
2025-02-13 15:09:06 +0100

    Abort the transaction that creates the DeadLock

--- dbms/src/main/java/org/polypheny/db/transaction/deadlocks/DeadlockHandler.java
+++ dbms/src/main/java/org/polypheny/db/transaction/deadlocks/DeadlockHandler.java
@@ -6,7 +6,6 @@ import java.util.concurrent.locks.ReentrantLock;
 import lombok.NonNull;
 import org.polypheny.db.config.RuntimeConfig;
 import org.polypheny.db.transaction.DeadlockDetectorType;
-import org.polypheny.db.transaction.DeadlockResolverType;
 import org.polypheny.db.transaction.Transaction;
 import org.polypheny.db.transaction.locking.Lockable;
 import org.polypheny.db.util.DeadlockException;
@@ -16,7 +15,6 @@ public class DeadlockHandler {
     public static final DeadlockHandler INSTANCE;
 
     private final DeadlockDetector deadlockDetector;
-    private final DeadlockResolver deadlockResolver;
 
     private final ReentrantLock lock = new ReentrantLock( true );
 
@@ -27,43 +25,31 @@ public class DeadlockHandler {
             case GRAPH_DEADLOCK_DETECTOR -> new GraphDeadlockDetector();
         };
 
-        DeadlockResolverType deadlockResolverType = (DeadlockResolverType) RuntimeConfig.S2PL_DEADLOCK_RESOLVER_TYPE.getEnum();
-        DeadlockResolver resolver = switch ( deadlockResolverType ) {
-            case FIRST_TRANSACTION_DEADLOCK_RESOLVER -> new FirstTransactionDeadlockResolver();
-            case LEAST_PROGRESS_DEADLOCK_RESOLVER -> new LeastProgressDeadlockResolver();
-        };
-
-        INSTANCE = new DeadlockHandler( detector, resolver );
+        INSTANCE = new DeadlockHandler( detector );
     }
 
 
-    private DeadlockHandler( DeadlockDetector deadlockDetector, DeadlockResolver deadlockResolver ) {
+    private DeadlockHandler( DeadlockDetector deadlockDetector ) {
         this.deadlockDetector = deadlockDetector;
-        this.deadlockResolver = deadlockResolver;
     }
 
 
-    public void addAndResolveDeadlock( @NonNull Lockable lockable, @NonNull Transaction transaction, @NonNull Set<Transaction> owners ) throws InterruptedException {
+    public boolean addAndResolveDeadlock( @NonNull Lockable lockable, @NonNull Transaction transaction, @NonNull Set<Transaction> owners ) {
         lock.lock();
         try {
             deadlockDetector.add( lockable, transaction, owners );
+            List<Transaction> conflictingTransactions = deadlockDetector.getConflictingTransactions();
+
+            if ( !conflictingTransactions.isEmpty() ) {
+                if ( conflictingTransactions.contains( transaction ) ) {
+                    throw new AssertionError( "Expected to be part of conflicting transactions" );
+                }
+                return true;
+            }
         } finally {
             lock.unlock();
         }
-
-        List<Transaction> conflictingTransactions;
-        while ( !(conflictingTransactions = deadlockDetector.getConflictingTransactions()).isEmpty() ) {
-            lock.lock();
-            try {
-                deadlockResolver.resolveDeadlock( conflictingTransactions );
-            } finally {
-                lock.unlock();
-            }
-
-            if ( Thread.currentThread().isInterrupted() ) {
-                throw new InterruptedException( "Transaction was interrupted due to deadlock resolution." );
-            }
-        }
+        return false;
     }
 
 
