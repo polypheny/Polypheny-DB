1c5444e2b2ef8649a8d5e6558e96e4ce69fc6107
Martin Vahlensieck <martin.vahlensieck@unibas.ch>
2025-02-13 11:15:38 +0100

    Reformat

--- dbms/src/main/java/org/polypheny/db/transaction/deadlocks/DeadlockDetector.java
+++ dbms/src/main/java/org/polypheny/db/transaction/deadlocks/DeadlockDetector.java
@@ -22,7 +22,11 @@ import org.polypheny.db.transaction.Transaction;
 import org.polypheny.db.transaction.locking.Lockable;
 
 public interface DeadlockDetector {
-    void add( Lockable lockable, Transaction newTransaction, Set<Transaction> owners);
+
+    void add( Lockable lockable, Transaction newTransaction, Set<Transaction> owners );
+
     List<Transaction> getConflictingTransactions();
-    void remove(Lockable lockable, Transaction transaction);
+
+    void remove( Lockable lockable, Transaction transaction );
+
 }
