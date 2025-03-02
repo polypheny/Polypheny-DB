52615364dcb86f9d019fcbd28a9e7a98e60094f4
Tobias Hafner <mail@tobiashafner.ch>
2025-03-02 15:29:51 +0100

    Start using new getters

--- core/src/main/java/org/polypheny/db/transaction/mvcc/IdentifierUtils.java
+++ core/src/main/java/org/polypheny/db/transaction/mvcc/IdentifierUtils.java
@@ -118,12 +118,6 @@ public class IdentifierUtils {
         );
     }
 
-    public static boolean isIdentifier( AlgDataTypeField field ) {
-        if ( field.getName().equals( IDENTIFIER_KEY ) ) {
-            return true;
-        }
-        return field.getName().equals( VERSION_KEY );
-    }
 
     public static boolean isIdentifier( AlgDataTypeField field ) {
         if ( field.getName().equals( IDENTIFIER_KEY ) ) {
