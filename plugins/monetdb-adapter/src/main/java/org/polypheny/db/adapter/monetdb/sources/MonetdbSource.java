7b28a7676ff6f49acc1aac2674dac0ddae5df8f8
romanost03 <roman.ostermiller@stud.unibas.ch>
2025-05-08 17:32:16 +0200

    Changes in table setting of monetdb adapter and override of "getExportedColumns" in monetdb-adapter. The adapter gets deployed now and shows data.

--- plugins/monetdb-adapter/src/main/java/org/polypheny/db/adapter/monetdb/sources/MonetdbSource.java
+++ plugins/monetdb-adapter/src/main/java/org/polypheny/db/adapter/monetdb/sources/MonetdbSource.java
@@ -17,7 +17,12 @@
 package org.polypheny.db.adapter.monetdb.sources;
 
 
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -29,6 +34,8 @@ import org.polypheny.db.adapter.annotations.AdapterSettingInteger;
 import org.polypheny.db.adapter.annotations.AdapterSettingString;
 import org.polypheny.db.adapter.jdbc.JdbcTable;
 import org.polypheny.db.adapter.jdbc.connection.ConnectionFactory;
+import org.polypheny.db.adapter.jdbc.connection.ConnectionHandler;
+import org.polypheny.db.adapter.jdbc.connection.ConnectionHandlerException;
 import org.polypheny.db.adapter.jdbc.connection.TransactionalConnectionFactory;
 import org.polypheny.db.adapter.jdbc.sources.AbstractJdbcSource;
 import org.polypheny.db.adapter.monetdb.MonetdbSqlDialect;
@@ -36,8 +43,12 @@ import org.polypheny.db.catalog.entity.allocation.AllocationTableWrapper;
 import org.polypheny.db.catalog.entity.logical.LogicalTableWrapper;
 import org.polypheny.db.catalog.entity.physical.PhysicalEntity;
 import org.polypheny.db.catalog.entity.physical.PhysicalTable;
+import org.polypheny.db.catalog.exceptions.GenericRuntimeException;
 import org.polypheny.db.prepare.Context;
 import org.polypheny.db.sql.language.SqlDialect;
+import org.polypheny.db.transaction.PUID;
+import org.polypheny.db.transaction.PolyXid;
+import org.polypheny.db.type.PolyType;
 
 
 @Slf4j
@@ -48,11 +59,11 @@ import org.polypheny.db.sql.language.SqlDialect;
         defaultMode = DeployMode.REMOTE)
 @AdapterSettingString(name = "host", defaultValue = "localhost", description = "Hostname or IP address of the remote MonetDB instance.", position = 1)
 @AdapterSettingInteger(name = "port", defaultValue = 50000, description = "JDBC port number on the remote MonetDB instance.", position = 2)
-@AdapterSettingString(name = "database", defaultValue = "polypheny", description = "JDBC port number on the remote MonetDB instance.", position = 3)
-@AdapterSettingString(name = "username", defaultValue = "polypheny", description = "Name of the database to connect to.", position = 4)
-@AdapterSettingString(name = "password", defaultValue = "polypheny", description = "Username to be used for authenticating at the remote instance.", position = 5)
+@AdapterSettingString(name = "database", defaultValue = "demo", description = "JDBC port number on the remote MonetDB instance.", position = 3)
+@AdapterSettingString(name = "username", defaultValue = "monetdb", description = "Name of the database to connect to.", position = 4)
+@AdapterSettingString(name = "password", defaultValue = "monetdb", description = "Username to be used for authenticating at the remote instance.", position = 5)
 @AdapterSettingInteger(name = "maxConnections", defaultValue = 25, description = "Password to be used for authenticating at the remote instance.")
-@AdapterSettingString(name = "table", defaultValue = "public.foo,public.bar", description = "Maximum number of concurrent JDBC connections.")
+@AdapterSettingString(name = "tables", defaultValue = "sys.testtable", description = "Maximum number of concurrent JDBC connections.")
 public class MonetdbSource extends AbstractJdbcSource {
 
     public MonetdbSource( final long storeId, final String uniqueName, final Map<String, String> settings, final DeployMode mode ) {
@@ -126,6 +137,111 @@ public class MonetdbSource extends AbstractJdbcSource {
         adapterCatalog.replacePhysical( physical );
 
         return List.of( physical );
+    }
+
+
+    @Override
+    public Map<String, List<ExportedColumn>> getExportedColumns() {
+        Map<String, List<ExportedColumn>> map = new HashMap<>();
+        PolyXid xid = PolyXid.generateLocalTransactionIdentifier( PUID.EMPTY_PUID, PUID.EMPTY_PUID );
+        try {
+            ConnectionHandler connectionHandler = connectionFactory.getOrCreateConnectionHandler( xid );
+            java.sql.Statement statement = connectionHandler.getStatement();
+            Connection connection = statement.getConnection();
+            DatabaseMetaData dbmd = connection.getMetaData();
+
+            String[] tables = settings.get( "tables" ).split( "," );
+            for ( String str : tables ) {
+                String[] names = str.split( "\\." );
+                if ( names.length == 0 || names.length > 2 || (requiresSchema() && names.length == 1) ) {
+                    throw new GenericRuntimeException( "Invalid table name: " + str );
+                }
+                String tableName;
+                String schemaPattern;
+                if ( requiresSchema() ) {
+                    schemaPattern = names[0];
+                    tableName = names[1];
+                } else {
+                    schemaPattern = null;
+                    tableName = names[0];
+                }
+                List<String> primaryKeyColumns = new ArrayList<>();
+                try ( ResultSet row = dbmd.getPrimaryKeys( null, schemaPattern, tableName ) ) {
+                    while ( row.next() ) {
+                        primaryKeyColumns.add( row.getString( "COLUMN_NAME" ) );
+                    }
+                }
+                try ( ResultSet row = dbmd.getColumns( null, schemaPattern, tableName, "%" ) ) {
+                    List<ExportedColumn> list = new ArrayList<>();
+                    while ( row.next() ) {
+                        PolyType type = PolyType.getNameForJdbcType( row.getInt( "DATA_TYPE" ) );
+                        Integer length = null;
+                        Integer scale = null;
+                        Integer dimension = null;
+                        Integer cardinality = null;
+                        switch ( type ) {
+                            case BOOLEAN:
+                            case TINYINT:
+                            case SMALLINT:
+                            case INTEGER:
+                            case BIGINT:
+                            case FLOAT:
+                            case REAL:
+                            case DOUBLE:
+                            case DATE:
+                                break;
+                            case DECIMAL:
+                                length = row.getInt( "COLUMN_SIZE" );
+                                scale = row.getInt( "DECIMAL_DIGITS" );
+                                break;
+                            case TIME:
+                                length = row.getInt( "DECIMAL_DIGITS" );
+                                if ( length > 3 ) {
+                                    throw new GenericRuntimeException( "Unsupported precision for data type time: " + length );
+                                }
+                                break;
+                            case TIMESTAMP:
+                                length = row.getInt( "DECIMAL_DIGITS" );
+                                if ( length > 3 ) {
+                                    throw new GenericRuntimeException( "Unsupported precision for data type timestamp: " + length );
+                                }
+                                break;
+                            case CHAR:
+                            case VARCHAR:
+                                type = PolyType.VARCHAR;
+                                length = row.getInt( "COLUMN_SIZE" );
+                                break;
+                            case BINARY:
+                            case VARBINARY:
+                                type = PolyType.VARBINARY;
+                                length = row.getInt( "COLUMN_SIZE" );
+                                break;
+                            default:
+                                throw new GenericRuntimeException( "Unsupported data type: " + type.getName() );
+                        }
+                        list.add( new ExportedColumn(
+                                row.getString( "COLUMN_NAME" ).toLowerCase(),
+                                type,
+                                null,
+                                length,
+                                scale,
+                                dimension,
+                                cardinality,
+                                row.getString( "IS_NULLABLE" ).equalsIgnoreCase( "YES" ),
+                                requiresSchema() ? row.getString( "TABLE_SCHEM" ) : row.getString( "TABLE_CAT" ),
+                                row.getString( "TABLE_NAME" ),
+                                row.getString( "COLUMN_NAME" ),
+                                row.getInt( "ORDINAL_POSITION" ),
+                                primaryKeyColumns.contains( row.getString( "COLUMN_NAME" ) )
+                        ) );
+                    }
+                    map.put( tableName, list );
+                }
+            }
+        } catch ( SQLException | ConnectionHandlerException e ) {
+            throw new GenericRuntimeException( "Exception while collecting schema information!" + e );
+        }
+        return map;
     }
 
 }
