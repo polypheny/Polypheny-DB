275a50fdb77de129e3e894cb0a05a2fcf01ec895
Yanick Spichty <yanick.spichty@gmail.com>
2026-04-03 11:34:56 +0200

    DEBUG: Change default registry and log level

--- core/src/main/java/org/polypheny/db/config/RuntimeConfig.java
+++ core/src/main/java/org/polypheny/db/config/RuntimeConfig.java
@@ -433,7 +433,7 @@ public enum RuntimeConfig {
     DOCKER_CONTAINER_REGISTRY(
             "docker/defaultContainerRegistry",
             "The default container registry to be used when pull container. Default is Docker Hub.",
-            "docker.io",
+            "localhost:3000",
             ConfigType.STRING,
             "dockerGeneralGroup" ),
 
