aa2758e7609aa00007819b8d01f9b1ac6564512f
Yanick Spichty <yanick.spichty@gmail.com>
2026-04-12 13:28:56 +0200

    Revert "DEBUG: Change default postgres adapter image to align with polypheny available docker images"
    This reverts commit e3de020c0e75edbd5d8838d8caaef28f1fd4026b.

--- plugins/postgres-adapter/src/main/java/org/polypheny/db/adapter/postgres/store/PostgresqlStore.java
+++ plugins/postgres-adapter/src/main/java/org/polypheny/db/adapter/postgres/store/PostgresqlStore.java
@@ -83,7 +83,7 @@ import org.polypheny.db.util.PasswordGenerator;
 @AdapterSettingList(
         name        = "imageVariant",
         options     = { "Default", "pgvector", "PostGIS", "pgvector & PostGIS" },
-        defaultValue = "Default",
+        defaultValue = "pgvector & PostGIS",
         position    = 7,
         description = "PostgreSQL Docker image variant to deploy.",
         appliesTo   = DeploySetting.DOCKER
@@ -147,7 +147,6 @@ public class PostgresqlStore extends AbstractJdbcStore {
             }
         }
         dialect.addSupportedFeatures( variant.features );
-        dialect.addSupportedFeatures( java.util.Set.of( PostgresqlFeature.POSTGIS ) );
 
         return createConnectionFactory();
     }
