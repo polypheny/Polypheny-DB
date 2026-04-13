86aea3ff36385fe496bb1c9724c5af3130d6f616
Yanick Spichty <yanick.spichty@gmail.com>
2026-04-13 22:24:54 +0200

    Implement PolyFloatList and add custom activej serializer

--- core/src/main/java/org/polypheny/db/type/entity/PolyFloatList.java
+++ core/src/main/java/org/polypheny/db/type/entity/PolyFloatList.java
@@ -16,190 +16,380 @@
 
 package org.polypheny.db.type.entity;
 
+import io.activej.serializer.BinaryInput;
+import io.activej.serializer.BinaryOutput;
+import io.activej.serializer.BinarySerializer;
+import io.activej.serializer.CompatibilityLevel;
+import io.activej.serializer.CorruptedDataException;
+import io.activej.serializer.def.SimpleSerializerDef;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import org.apache.calcite.linq4j.tree.Expression;
+import org.apache.calcite.linq4j.tree.Expressions;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.polypheny.db.type.PolySerializable;
 import org.polypheny.db.type.PolyType;
 import org.polypheny.db.type.entity.numerical.PolyFloat;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.NoSuchElementException;
 
+@EqualsAndHashCode(callSuper = false)
 public class PolyFloatList<E extends PolyFloat> extends PolyList<E> {
 
+    @Getter
+    private float[] value;
 
-    public PolyFloatList( PolyType type ) {
-        super( type );
+    public PolyFloatList( ) {
+        super( PolyType.ARRAY );
+        this.value = new float[0];
+    }
+
+
+    public PolyFloatList( float[] value ) {
+        super( PolyType.ARRAY );
+        this.value = value;
     }
 
 
     @Override
     public int size() {
-        return 0;
+        return value.length;
     }
 
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return value.length == 0;
     }
 
 
     @Override
     public boolean contains( Object o ) {
+        if ( o instanceof PolyFloat pf ) {
+            for ( float f : value ) {
+                if ( pf.equals( PolyFloat.of( f ) ) ) return true;
+            }
+        } else if ( o instanceof Float fl ) {
+            for ( float f : value ) {
+                if ( fl.equals( f ) ) return true;
+            }
+        }
         return false;
     }
 
 
     @Override
     public @NotNull Iterator<E> iterator() {
-        return null;
+        return new Iterator<>() {
+            private int cursor = 0;
+
+            @Override
+            public boolean hasNext() {
+                return cursor < value.length;
+            }
+
+            @Override
+            @SuppressWarnings("unchecked")
+            public E next() {
+                if ( !hasNext() ) throw new NoSuchElementException();
+                return (E) PolyFloat.of( value[cursor++] );
+            }
+        };
     }
 
 
     @Override
     public @NotNull Object[] toArray() {
-        return new Object[0];
+        Object[] arr = new Object[value.length];
+        for ( int i = 0; i < value.length; i++ ) {
+            arr[i] = PolyFloat.of( value[i] );
+        }
+        return arr;
     }
 
 
     @Override
+    @SuppressWarnings("unchecked")
     public @NotNull <T> T[] toArray( @NotNull T[] a ) {
-        return null;
+        int size = size();
+        if ( a.length < size ) {
+            return (T[]) Arrays.copyOf( toArray(), size, a.getClass() );
+        }
+        System.arraycopy( toArray(), 0, a, 0, size );
+        if ( a.length > size ) {
+            a[size] = null;
+        }
+        return a;
     }
 
 
     @Override
     public boolean add( E e ) {
-        return false;
+        float[] newArray = Arrays.copyOf( value, value.length + 1 );
+        newArray[value.length] = e.floatValue();
+        value = newArray;
+        return true;
     }
 
 
     @Override
     public boolean remove( Object o ) {
+        int index = indexOf( o );
+        if ( index >= 0 ) {
+            remove( index );
+            return true;
+        }
         return false;
     }
 
 
     @Override
     public boolean containsAll( @NotNull Collection<?> c ) {
-        return false;
+        for ( Object e : c ) {
+            if ( !contains( e ) ) return false;
+        }
+        return true;
     }
 
 
     @Override
     public boolean addAll( @NotNull Collection<? extends E> c ) {
-        return false;
+        if ( c.isEmpty() ) return false;
+        float[] newArray = Arrays.copyOf( value, value.length + c.size() );
+        int i = value.length;
+        for ( E e : c ) {
+            newArray[i++] = e.floatValue();
+        }
+        value = newArray;
+        return true;
     }
 
 
     @Override
     public boolean addAll( int index, @NotNull Collection<? extends E> c ) {
-        return false;
+        if ( index < 0 || index > value.length ) throw new IndexOutOfBoundsException();
+        if ( c.isEmpty() ) return false;
+
+        float[] newArray = new float[value.length + c.size()];
+        System.arraycopy( value, 0, newArray, 0, index );
+
+        int i = index;
+        for ( E e : c ) {
+            newArray[i++] = e.floatValue();
+        }
+
+        System.arraycopy( value, index, newArray, index + c.size(), value.length - index );
+        value = newArray;
+        return true;
     }
 
 
     @Override
     public boolean removeAll( @NotNull Collection<?> c ) {
-        return false;
+        boolean modified = false;
+        for ( Object e : c ) {
+            while ( remove( e ) ) {
+                modified = true;
+            }
+        }
+        return modified;
     }
 
 
     @Override
     public boolean retainAll( @NotNull Collection<?> c ) {
-        return false;
+        boolean modified = false;
+        for ( int i = value.length - 1; i >= 0; --i ) {
+            if ( !c.contains( get( i ) ) ) {
+                remove( i );
+                modified = true;
+            }
+        }
+        return modified;
     }
 
 
     @Override
     public void clear() {
-
+        value = new float[0];
     }
 
 
     @Override
+    @SuppressWarnings("unchecked")
     public E get( int index ) {
-        return null;
+        if ( index < 0 || index >= value.length ) throw new IndexOutOfBoundsException();
+        return (E) PolyFloat.of( value[index] );
     }
 
 
     @Override
     public E set( int index, E element ) {
-        return null;
+        if ( index < 0 || index >= value.length ) throw new IndexOutOfBoundsException();
+        E old = get( index );
+        value[index] = element.floatValue();
+        return old;
     }
 
 
     @Override
     public void add( int index, E element ) {
-
+        if ( index < 0 || index > value.length ) throw new IndexOutOfBoundsException();
+        float[] newArray = new float[value.length + 1];
+        System.arraycopy( value, 0, newArray, 0, index );
+        newArray[index] = element.floatValue();
+        System.arraycopy( value, index, newArray, index + 1, value.length - index );
+        value = newArray;
     }
 
 
     @Override
     public E remove( int index ) {
-        return null;
+        if ( index < 0 || index >= value.length ) throw new IndexOutOfBoundsException();
+        E old = get( index );
+        float[] newArray = new float[value.length - 1];
+        System.arraycopy( value, 0, newArray, 0, index );
+        System.arraycopy( value, index + 1, newArray, index, value.length - index - 1 );
+        value = newArray;
+        return old;
     }
 
 
     @Override
     public int indexOf( Object o ) {
-        return 0;
+        for ( int i = 0; i < value.length; ++i ) {
+            if ( get( i ).equals( o ) ) return i;
+        }
+        return -1;
     }
 
 
     @Override
     public int lastIndexOf( Object o ) {
-        return 0;
+        for ( int i = value.length - 1; i >= 0; --i ) {
+            if ( get( i ).equals( o ) ) return i;
+        }
+        return -1;
     }
 
 
     @Override
     public @NotNull ListIterator<E> listIterator() {
-        return null;
+        return listIterator( 0 );
     }
 
 
     @Override
     public @NotNull ListIterator<E> listIterator( int index ) {
-        return null;
+        List<E> list = new ArrayList<>();
+        for ( int i = 0; i < size(); i++ ) {
+            list.add( get( i ) );
+        }
+        return list.listIterator( index );
     }
 
 
     @Override
     public @NotNull List<E> subList( int fromIndex, int toIndex ) {
-        return List.of();
+        if ( fromIndex < 0 || toIndex > value.length || fromIndex > toIndex ) {
+            throw new IndexOutOfBoundsException();
+        }
+        List<E> sublist = new ArrayList<>( toIndex - fromIndex );
+        for ( int i = fromIndex; i < toIndex; ++i ) {
+            sublist.add( get( i ) );
+        }
+        return sublist;
     }
 
 
     @Override
     public @Nullable Long deriveByteSize() {
-        return 0L;
+        return (long) value.length * Float.BYTES;
     }
 
 
     @Override
     public Object toJava() {
-        return null;
+        return Arrays.asList( toArray() );
     }
 
 
     @Override
     public int compareTo( @NotNull PolyValue o ) {
-        return 0;
+        if ( !o.isList() ) return -1;
+        PolyList<?> other = o.asList();
+        int minSize = Math.min( this.size(), other.size() );
+        for ( int i = 0; i < minSize; i++ ) {
+            int cmp = this.get( i ).compareTo( other.get( i ) );
+            if ( cmp != 0 ) return cmp;
+        }
+        return Integer.compare( this.size(), other.size() );
     }
 
 
     @Override
     public Expression asExpression() {
-        return null;
+        List<Expression> list = new ArrayList<>();
+        for ( float f : value ) {
+            list.add( PolyFloat.of( f ).asExpression() );
+        }
+        return Expressions.call( PolyList.class, "ofExpression", list );
     }
 
 
     @Override
     public PolySerializable copy() {
-        return null;
+        return new PolyFloatList<>( Arrays.copyOf( value, value.length ) );
+    }
+
+
+    public float getRaw( int i ) {
+        return value[i];
+    }
+
+
+    public float[] getRawValue() {
+        return value;
+    }
+
+
+    public static class PolyFloatListSerializerDef extends SimpleSerializerDef<PolyFloatList<?>> {
+
+        @Override
+        protected BinarySerializer<PolyFloatList<?>> createSerializer( int version, CompatibilityLevel compatibilityLevel ) {
+            return new BinarySerializer<>() {
+
+                // size, value[0], value[1], ..., value[n], with n = item.size()
+                @Override
+                public void encode( BinaryOutput out, PolyFloatList<?> item ) {
+                    if ( item == null ) {
+                        out.writeInt( -1 );
+                        return;
+                    }
+                    out.writeInt( item.size() );
+                    for ( int i = 0; i < item.size(); ++i ) {
+                        out.writeFloat( item.getRaw( i ) );
+                    }
+                }
+
+                @Override
+                public PolyFloatList<?> decode( BinaryInput in ) throws CorruptedDataException {
+                    int size = in.readInt();
+                    if ( size == -1 ) return null;
+                    float[] arr = new float[size];
+                    for ( int i = 0; i < size; ++i ) {
+                        arr[i] = in.readFloat();
+                    }
+                    return new PolyFloatList<>( arr );
+                }
+            };
+        }
     }
 
 }
